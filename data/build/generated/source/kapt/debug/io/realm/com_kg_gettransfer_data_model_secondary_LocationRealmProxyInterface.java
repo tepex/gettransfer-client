478a61560e90dc628008ac62fc048bceb0beaa52
Tepex <k.terehov@key-g.com>
2018-07-26 18:52:37 +0300

    remove build/ from repo

--- data/build/generated/source/kapt/debug/io/realm/com_kg_gettransfer_data_model_secondary_LocationRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface com_kg_gettransfer_data_model_secondary_LocationRealmProxyInterface {
-    public String realmGet$name();
-    public void realmSet$name(String value);
-    public String realmGet$point();
-    public void realmSet$point(String value);
-}
--- data/build/generated/source/kapt/debug/io/realm/com_kg_gettransfer_data_model_secondary_PriceConvertedRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface com_kg_gettransfer_data_model_secondary_PriceConvertedRealmProxyInterface {
-    public String realmGet$defaultCurrency();
-    public void realmSet$defaultCurrency(String value);
-    public String realmGet$preferredCurrency();
-    public void realmSet$preferredCurrency(String value);
-}
