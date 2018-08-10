6e478d5ffedc9e638a7b2bd05bef6e4f226c6884
Tepex <k.terehov@key-g.com>
2018-08-10 18:02:39 +0300

    docs for Utils

--- domain/src/main/kotlin/com/kg/gettransfer/domain/Utils.kt
+++ domain/src/main/kotlin/com/kg/gettransfer/domain/Utils.kt
@@ -4,6 +4,16 @@ import kotlinx.coroutines.experimental.*
 
 /**
  * https://medium.com/@andrea.bresolin/playing-with-kotlin-in-android-coroutines-and-how-to-get-rid-of-the-callback-hell-a96e817c108b
+ * Sequential code
+ * 
+ * val result1 = asyncAwait { ...do something asynchronous... }
+ * val result2 = asyncAwait { ...do something asynchronous... }
+ * processResults(result1, result2)
+ *
+ * Parallel code
+ * val result1 = async { ...do something asynchronous... }
+ * val result2 = async { ...do something asynchronous... }
+ * processResults(result1.await(), result2.await())
  */
 class Utils(private val cc: CoroutineContexts) {
 	fun launchAsync(root: Job, block: suspend CoroutineScope.() -> Unit): Job {
