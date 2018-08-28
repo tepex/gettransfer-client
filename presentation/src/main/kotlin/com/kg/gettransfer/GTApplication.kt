fd0d069e7890b5e1ee11f265b8d4ba37a5d544a1
Tepex <k.terehov@key-g.com>
2018-08-28 17:53:33 +0300

    WIP

--- presentation/src/main/kotlin/com/kg/gettransfer/GTApplication.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/GTApplication.kt
@@ -3,7 +3,7 @@ package com.kg.gettransfer
 import android.app.Application
 import android.content.pm.ApplicationInfo
 import android.support.annotation.CallSuper
-//import android.support.multidex.MultiDexApplication
+import android.support.multidex.MultiDexApplication
 
 import com.kg.gettransfer.di.*
 
@@ -11,8 +11,8 @@ import org.koin.android.ext.android.startKoin
 
 import timber.log.Timber
 
-//class GTApplication: MultiDexApplication() {
-class GTApplication: Application() {
+class GTApplication: MultiDexApplication() {
+//class GTApplication: Application() {
 	@CallSuper
 	override fun onCreate() {
 		super.onCreate()
