f152be0f2088028201386380046eefb2517807da
Tepex <k.terehov@key-g.com>
2018-09-10 14:33:47 +0300

    refactor Navigator for SettingsActivity

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SettingsActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SettingsActivity.kt
@@ -13,14 +13,10 @@ import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.Toolbar
 
-import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 
 import com.kg.gettransfer.R
-
-import com.kg.gettransfer.domain.CoroutineContexts
-import com.kg.gettransfer.domain.interactor.ApiInteractor
 
 import com.kg.gettransfer.presentation.Screens
 import com.kg.gettransfer.presentation.model.CurrencyModel
@@ -34,36 +30,15 @@ import kotlinx.android.synthetic.main.toolbar.view.*
 
 import org.koin.android.ext.android.inject
 
-import ru.terrakok.cicerone.Navigator
-import ru.terrakok.cicerone.NavigatorHolder
-import ru.terrakok.cicerone.Router
-import ru.terrakok.cicerone.android.SupportAppNavigator
-
 import timber.log.Timber
 
-class SettingsActivity: MvpAppCompatActivity(), SettingsView {
+class SettingsActivity: BaseActivity(), SettingsView {
     @InjectPresenter
     internal lateinit var presenter: SettingsPresenter
     
-	private val apiInteractor: ApiInteractor by inject()
-	private val coroutineContexts: CoroutineContexts by inject()
-	private val navigatorHolder: NavigatorHolder by inject()
-	private val router: Router by inject()
-
     @ProvidePresenter
 	fun createSettingsPresenter(): SettingsPresenter = SettingsPresenter(coroutineContexts, router, apiInteractor)
 	
-    private val navigator: Navigator = object: SupportAppNavigator(this, Screens.NOT_USED) {
-		protected override fun createActivityIntent(context: Context, screenKey: String, data: Any?): Intent? {
-			when(screenKey) {
-				Screens.LOGIN -> return Intent(this@SettingsActivity, LoginActivity::class.java)
-			}
-			return null
-		}
-		
-		protected override fun createFragment(screenKey: String, data: Any?): Fragment? = null
-	}
-
     @CallSuper
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -78,18 +53,6 @@ class SettingsActivity: MvpAppCompatActivity(), SettingsView {
         (toolbar as Toolbar).setNavigationOnClickListener { presenter.onBackCommandClick() }
         
         btnSignOut.setOnClickListener { presenter.onLogout() }
-    }
-
-    @CallSuper
-    protected override fun onResume() {
-        super.onResume()
-        navigatorHolder.setNavigator(navigator)
-    }
-    
-    @CallSuper
-    protected override fun onPause() {
-        navigatorHolder.removeNavigator()
-        super.onPause()
     }
 
     override fun setCurrencies(currencies: List<CurrencyModel>) {
@@ -114,13 +77,5 @@ class SettingsActivity: MvpAppCompatActivity(), SettingsView {
     override fun setLocale(locale: String)             { tvSelectedLanguage.text = locale }
     override fun setDistanceUnit(distanceUnit: String) { tvSelectedDistanceUnits.text = distanceUnit }
     
-    override fun onBackPressed() {
-        presenter.onBackCommandClick()
-    }
-
-    override fun blockInterface(block: Boolean) {}
-
-    override fun setError(finish: Boolean, @StringRes errId: Int, vararg args: String?) {
-        Utils.showError(this, finish, getString(errId, *args))
-    }
+    override fun onBackPressed() { presenter.onBackCommandClick() }
 }
