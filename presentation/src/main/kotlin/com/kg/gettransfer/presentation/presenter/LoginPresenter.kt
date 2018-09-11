4f7d0e4d5fe71858899a348df97422938acd79d9
Tepex <k.terehov@key-g.com>
2018-09-11 12:16:06 +0300

    Refactor LoginPresenter

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/presenter/LoginPresenter.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/presenter/LoginPresenter.kt
@@ -4,17 +4,13 @@ import android.support.annotation.CallSuper
 import android.util.Patterns
 
 import com.arellomobile.mvp.InjectViewState
-import com.arellomobile.mvp.MvpPresenter
 
 import com.kg.gettransfer.R
 
-import com.kg.gettransfer.domain.AsyncUtils
 import com.kg.gettransfer.domain.CoroutineContexts
 import com.kg.gettransfer.domain.interactor.ApiInteractor
 import com.kg.gettransfer.domain.model.Account
 import com.kg.gettransfer.presentation.view.LoginView
-
-import kotlinx.coroutines.experimental.Job
 
 import ru.terrakok.cicerone.Router
 
@@ -22,16 +18,13 @@ import timber.log.Timber
 
 @InjectViewState
 class LoginPresenter(cc: CoroutineContexts, 
-                     private val router: Router,
-                     private val apiInteractor: ApiInteractor): MvpPresenter<LoginView>() {
+                     router: Router,
+                     apiInteractor: ApiInteractor): BasePresenter<LoginView>(cc, router, apiInteractor) {
 
     companion object {
         @JvmField val RESULT_CODE = 33
         @JvmField val RESULT_OK = 1
     }
-    
-    private val compositeDisposable = Job()
-    private val utils = AsyncUtils(cc)
     
     private var email: String? = null
     private var password: String? = null
@@ -58,15 +51,5 @@ class LoginPresenter(cc: CoroutineContexts,
         val checkEmail = email != null && Patterns.EMAIL_ADDRESS.matcher(email!!).matches()
         val checkPassword = password != null && password!!.length >= 6
         viewState.enableBtnLogin(checkEmail && checkPassword)
-    }
-    
-    @CallSuper
-    override fun onDestroy() {
-        compositeDisposable.cancel()
-        super.onDestroy()
-    }
-
-    fun onBackCommandClick() {
-        viewState.finish()
     }
 }
