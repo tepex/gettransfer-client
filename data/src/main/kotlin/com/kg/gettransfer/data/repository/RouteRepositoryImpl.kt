286c7c4ebd706fafcfd1ad1a4ff6419b283317f9
Tepex <k.terehov@key-g.com>
2018-09-27 20:08:53 +0300

    Start refactoring Preferences

--- data/src/main/kotlin/com/kg/gettransfer/data/repository/RouteRepositoryImpl.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/repository/RouteRepositoryImpl.kt
@@ -1,9 +1,8 @@
 package com.kg.gettransfer.data.repository
 
-import com.kg.gettransfer.data.RouteRemote
 import com.kg.gettransfer.domain.repository.RouteRepository
 
-class RouteRepositoryImpl(private val remote: RouteRemote): RouteRepository {
+class RouteRepositoryImpl(private val apiRepository: ApiRepositoryImpl): RouteRepository {
     override suspend fun getRouteInfo(from: String, to: String, withPrices: Boolean, returnWay: Boolean) =
-        remote.getRouteInfo(from, to, withPrices, returnWay)
+        apiRepository.getRouteInfo(from, to, withPrices, returnWay)
 }
