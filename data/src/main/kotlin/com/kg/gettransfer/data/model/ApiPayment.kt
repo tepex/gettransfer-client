870b985f2f5e00a387fcfc7a9462b9dcb349f41d
Alex Masliakov <a.maslyakov@key-g.com>
2018-10-03 17:10:51 +0300

    Revert "add payment"
    This reverts commit d51bc3b3b7d7ad2440d6822d53eb047456289d4a.

--- data/src/main/kotlin/com/kg/gettransfer/data/model/ApiPayment.kt
+++
@@ -1,7 +0,0 @@
-package com.kg.gettransfer.data.model
-
-import com.google.gson.annotations.SerializedName
-
-open class ApiPayment(@SerializedName("payment_id") val  paymentId: Long?,
-                      @SerializedName("pg_order_id") val pgOrderId: Long?,
-                      @SerializedName("without_redirect") val withoutRedirect: Boolean?)
