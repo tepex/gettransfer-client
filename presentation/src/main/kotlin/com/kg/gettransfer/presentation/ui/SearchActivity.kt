64e8e78ae751f93b7ef88b6307ba6755e48898c0
Sergei Tarasov <SerjT@admins-MacBook-Pro.local>
2018-10-08 14:03:51 +0300

    bounds and text in address From

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SearchActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SearchActivity.kt
@@ -17,6 +17,7 @@ import android.view.View
 
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
+
 import com.google.android.gms.maps.model.LatLngBounds
 
 import com.google.android.gms.maps.model.LatLngBounds
