94ed5635fa777415c2c0123feda1c7ffbcf381b7
Tepex <k.terehov@key-g.com>
2018-10-09 20:30:31 +0300

    Split Koin modules

--- presentation/src/main/kotlin/com/kg/gettransfer/di/Modules.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/di/Modules.kt
@@ -12,6 +12,7 @@ import com.kg.gettransfer.R
 
 import com.kg.gettransfer.data.logging.LoggingImpl
 
+import com.kg.gettransfer.data.CarrierTripRemote
 import com.kg.gettransfer.data.PreferencesCache
 import com.kg.gettransfer.data.RouteRemote
 import com.kg.gettransfer.data.SystemCache
@@ -27,19 +28,6 @@ import com.kg.gettransfer.domain.repository.*
 
 import com.kg.gettransfer.geo.GeoRepositoryImpl
 import com.kg.gettransfer.prefs.PreferencesImpl
-
-import com.kg.gettransfer.remote.ApiCore
-import com.kg.gettransfer.remote.RouteRemoteImpl
-import com.kg.gettransfer.remote.SystemRemoteImpl
-
-import com.kg.gettransfer.remote.mapper.AccountMapper as AccountRemoteMapper
-import com.kg.gettransfer.remote.mapper.CardGatewaysMapper as CardGatewaysRemoteMapper
-import com.kg.gettransfer.remote.mapper.ConfigsMapper as ConfigsRemoteMapper
-import com.kg.gettransfer.remote.mapper.CurrencyMapper as CurrencyRemoteMapper
-import com.kg.gettransfer.remote.mapper.LocaleMapper as LocaleRemoteMapper
-import com.kg.gettransfer.remote.mapper.PaypalCredentialsMapper as PaypalCredentialsRemoteMapper
-import com.kg.gettransfer.remote.mapper.RouteInfoMapper as RouteInfoRemoteMapper
-import com.kg.gettransfer.remote.mapper.TransportTypeMapper as TransportTypeRemoteMapper
 
 import kotlinx.coroutines.experimental.Dispatchers
 import kotlinx.coroutines.experimental.IO
@@ -67,27 +55,6 @@ val geoModule = module {
 
 val prefsModule = module {
     single { PreferencesImpl(get()) } bind PreferencesCache::class bind SystemCache::class
-}
-
-val remoteModule = module {
-    single {
-        val resources = (get() as Context).resources
-        ApiCore(get(), resources.getString(R.string.api_key), resources.getString(R.string.api_url))
-    }
-    single { RouteInfoRemoteMapper() }
-    single { RouteRemoteImpl(get(), get()) as RouteRemote }
-    single { AccountRemoteMapper() }
-    single { TransportTypeRemoteMapper() }
-    single { PaypalCredentialsRemoteMapper() }
-    single { LocaleRemoteMapper() }
-    single { CurrencyRemoteMapper() }
-    single { CardGatewaysRemoteMapper() }
-    single { ConfigsRemoteMapper(get(), get(), get(), get(), get()) }    
-    single { SystemRemoteImpl(get(), get(), get()) as SystemRemote }
-    
-    
-    single { CarrierTripRemoteMapper(get(), get()) as CarrierTripRemote }
-    single { CarrierTripRemoteImpl(get(), get()) as CarrierTripRemote }
 }
 
 val dataModule = module {
--- remote/src/main/kotlin/com/kg/gettransfer/remote/mapper/PassengerAccountMapper.kt
+++ remote/src/main/kotlin/com/kg/gettransfer/remote/mapper/PassengerAccountMapper.kt
@@ -1,15 +1,16 @@
 package com.kg.gettransfer.remote.mapper
 
 import com.kg.gettransfer.data.model.PassengerAccountEntity
+import com.kg.gettransfer.data.model.ProfileEntity
 
 import com.kg.gettransfer.remote.model.PassengerAccountModel
 
 /**
  * Map a [PassengerAccountModel] from a [PassengerAccountEntity] instance when data is moving between this later and the Data layer.
  */
-open class PassengerAccountMapper(private val profileMapper: ProfileMapper): EntityMapper<PassengerAccountModel, PassengerAccountEntity> {
+open class PassengerAccountMapper(): EntityMapper<PassengerAccountModel, PassengerAccountEntity> {
 
     override fun fromRemote(type: PassengerAccountModel) = 
-        PassengerAccountEntity(profileMapper.fromRemote(type.))
-    override fun toRemote(type: PassengerAccountEntity): PassengerAccountModel(type.name, type.point, type.placeId)
+        PassengerAccountEntity(ProfileEntity(type.fullName, type.email, type.phone), type.lastSeen)
+    override fun toRemote(type: PassengerAccountEntity): PassengerAccountModel { throw UnsupportedOperationException() }
 }
