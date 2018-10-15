2a1743df38a075c1548d344747f758379eaffd3e
Tepex <k.terehov@key-g.com>
2018-10-15 12:03:53 +0300

    CityPoint mapper

--- data/src/main/kotlin/com/kg/gettransfer/data/repository/RouteRepositoryImpl.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/repository/RouteRepositoryImpl.kt
@@ -5,12 +5,20 @@ import com.kg.gettransfer.data.RouteDataStore
 import com.kg.gettransfer.data.ds.RouteDataStoreFactory
 import com.kg.gettransfer.data.ds.RouteRemoteDataStore
 
+import com.kg.gettransfer.data.mapper.PointMapper
 import com.kg.gettransfer.data.mapper.RouteInfoMapper
+
+import com.kg.gettransfer.domain.model.Point
+import com.kg.gettransfer.domain.model.RouteInfo
 
 import com.kg.gettransfer.domain.repository.RouteRepository
 
 class RouteRepositoryImpl(private val factory: RouteDataStoreFactory,
-                          private val mapper: RouteInfoMapper): RouteRepository {
-    override suspend fun getRouteInfo(from: String, to: String, withPrices: Boolean, returnWay: Boolean) =
-        mapper.fromEntity(factory.retrieveRemoteDataStore().getRouteInfo(from, to, withPrices, returnWay))
+                          private val routeInfoMapper: RouteInfoMapper,
+                          private val pointMapper: PointMapper): RouteRepository {
+    override suspend fun getRouteInfo(from: Point, to: Point, withPrices: Boolean, returnWay: Boolean): RouteInfo {
+        val fromString = pointMapper.toEntity(from)
+        val toString = pointMapper.toEntity(to)
+        return routeInfoMapper.fromEntity(factory.retrieveRemoteDataStore().getRouteInfo(fromString, toString, withPrices, returnWay))
+    }
 }
