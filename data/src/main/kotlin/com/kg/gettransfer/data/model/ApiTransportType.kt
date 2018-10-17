052236f11b284d44a2f9233dad0782d94d03984d
Tepex <k.terehov@key-g.com>
2018-10-17 12:01:08 +0300

    remove old classes

--- data/src/main/kotlin/com/kg/gettransfer/data/model/ApiTransportType.kt
+++
@@ -1,8 +0,0 @@
-package com.kg.gettransfer.data.model
-
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-class ApiTransportType(@SerializedName("id") @Expose val id: String,
-                       @SerializedName("pax_max") @Expose val paxMax: Int,
-                       @SerializedName("luggage_max") @Expose val luggageMax: Int)
