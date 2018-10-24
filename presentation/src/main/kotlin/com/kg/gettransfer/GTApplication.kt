b46249bee11a6c036368a64813a77406a0940b08
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2018-10-24 18:30:01 +0300

    Delete CrashManager from dev flavor

--- presentation/src/main/kotlin/com/kg/gettransfer/GTApplication.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/GTApplication.kt
@@ -24,7 +24,7 @@ class GTApplication: MultiDexApplication() {
         if(BuildConfig.FLAVOR == "dev") {
             Timber.plant(FileLoggingTree(applicationContext))
             System.setProperty("kotlinx.coroutines.debug", "on")
-            CrashManager.register(this)
+            //DELETE CrashManager.register(this)
         }
         // Start Koin
         startKoin(this, listOf(ciceroneModule,
