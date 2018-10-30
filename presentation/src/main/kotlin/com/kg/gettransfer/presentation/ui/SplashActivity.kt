5edc8ecbc993f99c706bea43b7f294db541d9340
Tepex <k.terehov@key-g.com>
2018-10-30 09:47:42 +0300

    Fix crash on HTTP 500

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SplashActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SplashActivity.kt
@@ -34,71 +34,68 @@ import org.koin.android.ext.android.inject
 import timber.log.Timber
 
 class SplashActivity: AppCompatActivity() {
-	companion object {
-		@JvmField val PERMISSIONS = arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
-		@JvmField val PERMISSION_REQUEST = 2211
-	}
-	
-	private val compositeDisposable = Job()
-	private val coroutineContexts: CoroutineContexts by inject()
-	private val utils = AsyncUtils(coroutineContexts, compositeDisposable)
-	private val systemInteractor: SystemInteractor by inject()
-	
-	@CallSuper
-	protected override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && 
-			(!check(Manifest.permission.ACCESS_FINE_LOCATION) || 
-			 !check(Manifest.permission.ACCESS_COARSE_LOCATION))) {
-			ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST)
-			// show splash
-			Timber.d("Splash screen")
-			return
-		}
+    companion object {
+        @JvmField val PERMISSIONS = arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        @JvmField val PERMISSION_REQUEST = 2211
+    }
 
-		if (checkIsTaskRoot()) return
+    private val compositeDisposable = Job()
+    private val coroutineContexts: CoroutineContexts by inject()
+    private val utils = AsyncUtils(coroutineContexts, compositeDisposable)
+    private val systemInteractor: SystemInteractor by inject()
 
-		Timber.d(getString(R.string.title_starting_session))
-		Timber.d("Permissions granted!")
-		utils.launchAsyncTryCatchFinally({
-		    utils.asyncAwait { systemInteractor.coldStart() }
-			when(systemInteractor.lastMode) {
-				Screens.CARRIER_MODE -> startActivity(Intent(this@SplashActivity, CarrierTripsActivity::class.java))
-				Screens.PASSENGER_MODE -> startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-				else -> startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-			}
-		    finish()
-			//startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-		}, { e -> Timber.e(e); Utils.showError(this@SplashActivity, false, getString(R.string.err_server, e.message))
-			// @TODO: Показать ошибку. Учесть 401 — протухший ключ
-		}, { /*finish()*/ })
-	}
+    @CallSuper
+    protected override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && 
+            (!check(Manifest.permission.ACCESS_FINE_LOCATION) || 
+             !check(Manifest.permission.ACCESS_COARSE_LOCATION))) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST)
+            // show splash
+            Timber.d("Splash screen")
+            return
+        }
 
-	private fun checkIsTaskRoot(): Boolean {
-		if (!isTaskRoot
-				&& intent.hasCategory(Intent.CATEGORY_LAUNCHER)
-				&& intent.action != null
-				&& intent.action.equals(Intent.ACTION_MAIN)) {
-			finish()
-			return true
-		}
-		return false
-	}
+        if (checkIsTaskRoot()) return
 
-	@CallSuper
-	protected override fun onDestroy() {
-		compositeDisposable.cancel()
-		super.onDestroy()
-	}
+        Timber.d(getString(R.string.title_starting_session))
+        Timber.d("Permissions granted!")
+        utils.launchAsyncTryCatchFinally({
+            utils.asyncAwait { systemInteractor.coldStart() }
+            when(systemInteractor.lastMode) {
+                Screens.CARRIER_MODE -> startActivity(Intent(this@SplashActivity, CarrierTripsActivity::class.java))
+                Screens.PASSENGER_MODE -> startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                else -> startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+            finish()
+            //startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, { e -> Timber.e(e); Utils.showError(this@SplashActivity, false, getString(R.string.err_server, e.message))
+            // @TODO: Показать ошибку. Учесть 401 — протухший ключ
+        }, { /*finish()*/ })
+    }
 
-	private fun check(permission: String) =
-		ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
-	
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-		if(requestCode != PERMISSION_REQUEST) return
-		if(grantResults.size == 2 && 
-			grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-			grantResults[1] == PackageManager.PERMISSION_GRANTED) recreate()
-		else finish()
-	}
+    private fun checkIsTaskRoot(): Boolean {
+        if(!isTaskRoot && intent.hasCategory(Intent.CATEGORY_LAUNCHER) && intent.action != Intent.ACTION_MAIN) {
+            finish()
+            return true
+        }
+        return false
+    }
+
+    @CallSuper
+    protected override fun onDestroy() {
+        compositeDisposable.cancel()
+        super.onDestroy()
+    }
+
+    private fun check(permission: String) =
+        ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if(requestCode != PERMISSION_REQUEST) return
+        if(grantResults.size == 2 && 
+            grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+            grantResults[1] == PackageManager.PERMISSION_GRANTED) recreate()
+        else finish()
+    }
 }
