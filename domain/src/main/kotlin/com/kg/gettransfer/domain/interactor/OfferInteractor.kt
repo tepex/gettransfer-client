d12cf1d7889142ec52d4180d5b803dbe2f0a4a2c
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2018-11-13 16:59:40 +0300

    Don't sending request for offers when changing currency in settings

--- domain/src/main/kotlin/com/kg/gettransfer/domain/interactor/OfferInteractor.kt
+++ domain/src/main/kotlin/com/kg/gettransfer/domain/interactor/OfferInteractor.kt
@@ -9,8 +9,6 @@ class OfferInteractor(private val repository: OfferRepository) {
     lateinit var offers: List<Offer>
 
     suspend fun getOffers(transferId: Long) = repository.getOffers(transferId).also { offers = it .model}
-    
-    fun getOffer(id: Long) = offers.find { it.id == id }
-    
+    fun getOffer(id: Long) = offers.find { it.id == id }    
     fun newOffer(offer: Offer) = repository.newOffer(offer)
 }
