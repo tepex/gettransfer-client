c3b829797dd0a1b98786f5da9bffe0da22c112ed
Tepex <k.terehov@key-g.com>
2018-11-16 17:28:58 +0300

    Fix rebase mr-195

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/BaseActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/BaseActivity.kt
@@ -54,9 +54,6 @@ import ru.terrakok.cicerone.Router
 import ru.terrakok.cicerone.android.SupportAppNavigator
 
 import timber.log.Timber
-import android.app.Activity
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
 
 abstract class BaseActivity: MvpAppCompatActivity(), BaseView {
     internal val systemInteractor: SystemInteractor by inject()
@@ -77,12 +74,6 @@ abstract class BaseActivity: MvpAppCompatActivity(), BaseView {
         if(event.action == MotionEvent.ACTION_MOVE) hideKeyboardWithoutClearFocus(this, view)
         else return@OnTouchListener false
     }
-<<<<<<< HEAD
-    
-    private val compositeDisposable = Job()
-    private val utils = AsyncUtils(coroutineContexts, compositeDisposable)
-=======
->>>>>>> GAA-232 & GAA-257 (Hide keyboard when scrolling in CreateOrder & Search)
     
     private val inetReceiver = object: BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) = setNetworkAvailability(context)
@@ -148,10 +139,10 @@ abstract class BaseActivity: MvpAppCompatActivity(), BaseView {
         currentFocus?.showKeyboard()
     }
 
-    protected fun hideKeyboard() {
-        currentFocus?.let {
-            it.hideKeyboard()
-            it.clearFocus()
+    protected fun hideKeyboard(): Boolean {
+        currentFocus?.run {
+            hideKeyboard()
+            clearFocus()
         }
         return true
     }
