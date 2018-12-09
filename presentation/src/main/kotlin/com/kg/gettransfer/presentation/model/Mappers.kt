ecbc3811d8cc9aa0b78f4fba68d2cb86c0bc89bd
Tepex <k.terehov@key-g.com>
2018-12-09 18:04:29 +0300

    refactor(mapper): WIP. OfferMapper

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/model/Mappers.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/model/Mappers.kt
@@ -41,10 +41,6 @@ object Mappers : KoinComponent {
 
     fun getUserModel(type: Account) = UserModel(getProfileModel(type.user.profile), type.user.termsAccepted)
 
-    fun getProfileModel(type: Profile) = ProfileModel(type.fullName, type.email, type.phone)
-
-    fun getProfile(model: ProfileModel) = Profile(model.name, model.email, model.phone)
-
     fun getUser(model: UserModel) = User(getProfile(model.profile), model.termsAccepted)
 
     /*
@@ -55,25 +51,10 @@ object Mappers : KoinComponent {
                    groups: List<String>?,
                    carrierId: Long?) = Account(model, locale, currency, distanceUnit, groups, carrierId) */
 
-    fun getTransportTypeModel(type: TransportType, prices: Map<String, TransportPrice>?): TransportTypeModel {
-        val imageRes = R.drawable::class.members.find( { it.name == "ic_transport_type_${type.id}" } )
-        val imageId = (imageRes?.call() as Int?) ?: R.drawable.ic_transport_type_unknown
-        return TransportTypeModel(type.id,
-                                  getTransportTypeName(type.id),
-                                  imageId,
-                                  type.paxMax,
-                                  type.luggageMax,
-                                  prices?.get(type.id))
-    }
 
-    @StringRes
-    fun getTransportTypeName(id: String): Int {
-        val nameRes = R.string::class.members.find( { it.name == "LNG_TRANSPORT_${id.toUpperCase()}" } )
-        return (nameRes?.call() as Int?) ?: R.string.LNG_TRANSPORT_ECONOMY
-    }
 
     fun getCurrenciesModels(types: List<Currency>)        = types.map { CurrencyModel(it) }
-    fun getLocalesModels(types: List<Locale>)             = types.map { LocaleModel(it) }
+//    fun getLocalesModels(types: List<Locale>)             = types.map { LocaleModel(it) }
     fun getDistanceUnitsModels(types: List<DistanceUnit>) = types.map { DistanceUnitModel(it) }
 
     fun getEndpointModel(endpoint: Endpoint) = EndpointModel(endpoint)
@@ -169,21 +150,6 @@ object Mappers : KoinComponent {
                                                           paypalOnly)
 
 
-    fun getOfferModel(type: Offer) =
-        OfferModel(
-            type.id,
-            type.status,
-            type.wifi,
-            type.refreshments,
-            SystemUtils.formatDateTime(type.createdAt),
-            getPriceModel(type.price),
-            type.ratings?.let { getRatingsModel(it) },
-            type.passengerFeedback,
-            getCarrierModel(type.carrier),
-            getVehicleModel(type.vehicle),
-            type.driver?.let { getProfileModel(it) },
-            type.phoneToCall
-        )
 
     fun getCarrierTripModel(type: CarrierTrip) =
         CarrierTripModel(
@@ -202,35 +168,6 @@ object Mappers : KoinComponent {
             type.flightNumber,
             type.remainToPay
         )
-
-    private fun getRatingsModel(type: Ratings) = RatingsModel(type.average, type.vehicle, type.driver, type.fair)
-
-    private fun getMoneyModel(type: Money) = MoneyModel(type.default, type.preferred)
-
-    private fun getPriceModel(type: Price) = PriceModel(getMoneyModel(type.base),
-                                                type.withoutDiscount?.let { getMoneyModel(it)},
-                                                type.percentage30,
-                                                type.percentage70,
-                                                "%.2f".format(type.amount))
-
-    private fun getCarrierModel(type: Carrier) =
-        CarrierModel(
-            type.id,
-            type.profile?.let { getProfileModel(it) },
-            type.approved,
-            type.completedTransfers,
-            getLocalesModels(type.languages),
-            getRatingsModel(type.ratings),
-            type.canUpdateOffers
-        )
-
-    private fun getVehicleModel(type: Vehicle) =
-        VehicleModel(VehicleBaseModel(type.vehicleBase.name, type.vehicleBase.registrationNumber),
-                     type.year,
-                     type.color,
-                     getTransportTypeModel(type.transportType, null),
-                     type.photos)
-
 
     private fun checkDistance(from: Point, to: Point) = (point2Location(from).distanceTo(point2Location(to)) / 1000).toInt()
 }
