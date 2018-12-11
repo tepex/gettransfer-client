7a5a0053ab51ac09cdcf473cba58c07406e27da2
Tepex <k.terehov@key-g.com>
2018-12-11 17:15:22 +0300

    fix: cherry-pick conflicts from gaa-383/redesign_driver_mode

--- domain/src/main/kotlin/com/kg/gettransfer/domain/model/CarrierTrip.kt
+++ domain/src/main/kotlin/com/kg/gettransfer/domain/model/CarrierTrip.kt
@@ -2,25 +2,27 @@ package com.kg.gettransfer.domain.model
 
 import java.util.Date
 
-data class CarrierTrip(val id: Long,
-                       val transferId: Long,
-                       val from: CityPoint,
-                       val to: CityPoint,
-                       val dateLocal: Date,
-                       val duration: Int?,
-                       val distance: Int?,
-                       val time: Int,
-                       val childSeats: Int,
-                       val comment: String?,
-                       val waterTaxi: Boolean,
-                       val price: String,
-                       val vehicle: VehicleBase,
-                       val pax: Int?,
-                       val nameSign: String?,
-                       val flightNumber: String?,
-                       val paidSum: String?,
-                       val remainToPay: String?,
-                       val paidPercentage: Int?,
-                       val passengerAccount: PassengerAccount?)
+data class CarrierTrip(
+    val id: Long,
+    val transferId: Long,
+    val from: CityPoint,
+    val to: CityPoint,
+    val dateLocal: Date,
+    val duration: Int?,
+    val distance: Int?,
+    val time: Int?,
+    val childSeats: Int,
+    val comment: String?,
+    val waterTaxi: Boolean,
+    val price: String,
+    val vehicle: VehicleBase,
+    val pax: Int?,
+    val nameSign: String?,
+    val flightNumber: String?,
+    val paidSum: String?,
+    val remainToPay: String?,
+    val paidPercentage: Int?,
+    val passengerAccount: PassengerAccount?
+)
 
 data class PassengerAccount(val profile: Profile, val lastSeen: Date)
