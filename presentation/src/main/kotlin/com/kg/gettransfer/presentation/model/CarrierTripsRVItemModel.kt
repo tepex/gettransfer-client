318646d97dc6282019f7c33ee7d335dfd8022446
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2018-12-21 12:22:23 +0300

    Types for CarrierTripsRVItemModel to enum

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/model/CarrierTripsRVItemModel.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/model/CarrierTripsRVItemModel.kt
@@ -1,16 +1,16 @@
 package com.kg.gettransfer.presentation.model
 
 data class CarrierTripsRVItemModel(
-    val type: String,
+    val type: Type,
     val titleText: String?,
     val isToday: Boolean?,
     val item: CarrierTripBaseModel?
 ) {
 
-    companion object {
-        const val TYPE_TITLE          = "type_title"
-        const val TYPE_SUBTITLE       = "type_subtitle"
-        const val TYPE_ITEM           = "type_item"
-        const val TYPE_END_TODAY_VIEW = "type_end_today_view"
+    enum class Type {
+        TITLE,
+        SUBTITLE,
+        ITEM,
+        END_TODAY_VIEW
     }
 }
