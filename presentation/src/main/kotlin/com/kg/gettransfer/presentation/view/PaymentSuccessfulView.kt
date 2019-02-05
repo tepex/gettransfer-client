b941779c4ba7f53f1795cff06e14be828ada5de8
Evgeny <e.gromov@key-g.com>
2019-02-05 18:41:15 +0300

    gaa-680. pin for hourly transfer added to the successful screen

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/PaymentSuccessfulView.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/PaymentSuccessfulView.kt
@@ -1,8 +1,11 @@
 package com.kg.gettransfer.presentation.view
 
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.model.LatLng
 import com.kg.gettransfer.presentation.model.PolylineModel
 
 interface PaymentSuccessfulView: BaseView {
     fun setRoute(polyline: PolylineModel)
     fun setRemainTime(days: Int, hours: Int, minutes: Int)
+    fun setPinHourlyTransfer(point: LatLng, cameraUpdate: CameraUpdate)
 }
