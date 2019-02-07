9ffe89abb7aac813496bbef4c3ad54e5a2c3cbe4
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2019-02-07 17:36:47 +0300

    Fix crash with click in offer notification without rating

--- data/src/main/kotlin/com/kg/gettransfer/data/model/RatingsEntity.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/model/RatingsEntity.kt
@@ -1,14 +1,15 @@
 package com.kg.gettransfer.data.model
 
+import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 
 @Serializable
 data class RatingsEntity(
-    @SerialName(AVERAGE) val average: Float?,
-    @SerialName(VEHICLE) val vehicle: Float?,
-    @SerialName(DRIVER) val driver:   Float?,
-    @SerialName(FAIR) val fair:       Float?
+        @SerialName(AVERAGE)           val average: Float?,
+        @Optional @SerialName(VEHICLE) val vehicle: Float? = null,
+        @Optional @SerialName(DRIVER)  val driver: Float? = null,
+        @Optional @SerialName(FAIR)    val fair: Float? = null
 ) {
 
     companion object {
