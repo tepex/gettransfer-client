8de92d4fc775abd1637e016609a2af6e8c1cdc83
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2019-02-19 19:16:56 +0300

    Fix GAA-765 (crash after rate transfer)

--- data/src/main/kotlin/com/kg/gettransfer/data/repository/ReviewRepositoryImpl.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/repository/ReviewRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.kg.gettransfer.data.repository
 
+import com.kg.gettransfer.data.RemoteException
 import com.kg.gettransfer.data.ds.ReviewDataStoreRemote
 import com.kg.gettransfer.data.mapper.ExceptionMapper
 import com.kg.gettransfer.data.mapper.ReviewRateMapper
@@ -14,14 +15,12 @@ class ReviewRepositoryImpl(private val remote: ReviewDataStoreRemote) : ReviewRe
     private val rateMapper = get<ReviewRateMapper>()
 
     override suspend fun rateTrip(offerId: Long, list: List<ReviewRate>, comment: String): Result<Unit> {
-        var res = Result(Unit)
-        list.forEach { rate ->
-            retrieveRemoteEntity { remote.sendReview(offerId, rateMapper.toEntity(rate)) }
-                .also { r ->
-                    r.error?.let { res = Result(Unit, ExceptionMapper.map(it)) }
-                }
-        }
-        remote.sendFeedBackComment(offerId, comment)
-        return res
+        return try {
+            list.forEach { rate ->
+                retrieveRemoteEntity { remote.sendReview(offerId, rateMapper.toEntity(rate)) }
+            }
+            remote.sendFeedBackComment(offerId, comment)
+            Result(Unit)
+        } catch (e: RemoteException) { Result(Unit, ExceptionMapper.map(e)) }
     }
 }
