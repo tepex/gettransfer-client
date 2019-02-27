96632b61a099af00716ad42e8211e0a2c61cf527
Evgeny <e.gromov@key-g.com>
2019-02-27 16:33:00 +0300

    gaa-802. added PhoneNumberFormatter(implementation and changing Android's PhoneNumberFormattingTextWatcher)

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/CreateOrderActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/CreateOrderActivity.kt
@@ -17,8 +17,6 @@ import android.support.v4.content.ContextCompat
 
 import android.support.v7.widget.LinearLayoutManager
 
-import android.telephony.PhoneNumberFormattingTextWatcher
-
 import android.text.InputFilter
 import android.text.InputType //don't delete
 import android.text.TextUtils //don't delete
@@ -55,6 +53,7 @@ import com.kg.gettransfer.utilities.Analytics.Companion.CAR_INFO_CLICKED
 import com.kg.gettransfer.utilities.Analytics.Companion.COMMENT_INPUT
 import com.kg.gettransfer.utilities.Analytics.Companion.DATE_TIME_CHANGED
 import com.kg.gettransfer.utilities.Analytics.Companion.OFFER_PRICE_FOCUSED
+import com.kg.gettransfer.utilities.PhoneNumberFormatter
 
 import java.util.Calendar
 
@@ -476,13 +475,9 @@ class CreateOrderActivity : BaseGoogleMapActivity(), CreateOrderView {
         user_name_field.field_input.onTextChanged        { presenter.setName(it.trim()) }
         email_field.field_input.onTextChanged            { presenter.setEmail(it.trim()) }
         phone_field.field_input.onTextChanged            {
-            if (it.isEmpty()) {
-                phone_field.field_input.setText("+")
-                phone_field.field_input.setSelection(1)
-            }
             presenter.setPhone("+".plus(it.replace(Regex("\\D"), "")))
         }
-        phone_field.field_input.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+        phone_field.field_input.addTextChangedListener(PhoneNumberFormatter(phone_field.field_input))
         flight_number_field.field_input.onTextChanged         { presenter.setFlightNumber(it.trim(), false) }
         flight_numberReturn_field.field_input.onTextChanged   { presenter.setFlightNumber(it.trim(), true) }
         promo_field.field_input.onTextChanged                 { presenter.setPromo(promo_field.field_input.text.toString()) }
