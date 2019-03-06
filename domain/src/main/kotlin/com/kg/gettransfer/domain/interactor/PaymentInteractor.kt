b1b6d87ea9a33aa96a14d4691721fbe77bf078d2
Evgeny <e.gromov@key-g.com>
2019-03-06 18:23:38 +0300

    gaa-656. confirm paypal payment

--- domain/src/main/kotlin/com/kg/gettransfer/domain/interactor/PaymentInteractor.kt
+++ domain/src/main/kotlin/com/kg/gettransfer/domain/interactor/PaymentInteractor.kt
@@ -9,4 +9,6 @@ class PaymentInteractor(private val repository: PaymentRepository) {
     suspend fun getPayment(paymentRequest: PaymentRequest) = repository.getPayment(paymentRequest)
     suspend fun changeStatusPayment(paymentStatusRequest: PaymentStatusRequest) =
         repository.changeStatusPayment(paymentStatusRequest)
+    suspend fun getBrainTreeToken() = repository.getBrainTreeToken()
+    suspend fun confirmPaypal(paymentId: Long, nonce: String) = repository.confirmPaypal(paymentId, nonce)
 }
