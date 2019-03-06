e0b1c4ec908e7bcb843c396ca3d23bf370441d4b
Evgeny <e.gromov@key-g.com>
2019-03-06 19:18:55 +0300

    gaa-656. fix

--- data/src/main/kotlin/com/kg/gettransfer/data/ds/PaymentDataStoreCache.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/ds/PaymentDataStoreCache.kt
@@ -1,29 +1,27 @@
 package com.kg.gettransfer.data.ds
 
-import com.kg.gettransfer.data.PaymentCache
 import com.kg.gettransfer.data.PaymentDataStore
-import com.kg.gettransfer.data.RemoteException
+import com.kg.gettransfer.data.model.*
 
-import com.kg.gettransfer.data.mapper.ExceptionMapper
-
-import com.kg.gettransfer.data.model.PaymentEntity
-import com.kg.gettransfer.data.model.PaymentRequestEntity
-import com.kg.gettransfer.data.model.PaymentStatusEntity
-import com.kg.gettransfer.data.model.PaymentStatusRequestEntity
-
-import org.koin.standalone.inject
 
 /**
  * Implementation of the [PaymentDataStore] interface to provide a means of communicating with the cache data source.
  */
 open class PaymentDataStoreCache: PaymentDataStore {
-    private val cache: PaymentCache by inject()
     
     override suspend fun createPayment(paymentRequest: PaymentRequestEntity): PaymentEntity {
         throw UnsupportedOperationException()
     }
     
     override suspend fun changeStatusPayment(paymentStatusRequest: PaymentStatusRequestEntity): PaymentStatusEntity {
+        throw UnsupportedOperationException()
+    }
+
+    override suspend fun getBraintreeToken(): BraintreeTokenEntity {
+        throw UnsupportedOperationException()
+    }
+
+    override suspend fun confirmPaypal(paymentId: Long, nonce: String): PaymentStatusEntity {
         throw UnsupportedOperationException()
     }
 }
