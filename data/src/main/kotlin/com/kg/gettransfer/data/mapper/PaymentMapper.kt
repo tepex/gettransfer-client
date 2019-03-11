fba05bee92cc5240076a43dc82fa4424f466a472
Evgeny <e.gromov@key-g.com>
2019-03-11 15:55:59 +0300

    gaa-656. added new model

--- data/src/main/kotlin/com/kg/gettransfer/data/mapper/PaymentMapper.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/mapper/PaymentMapper.kt
@@ -4,18 +4,20 @@ import com.kg.gettransfer.data.model.PaymentEntity
 
 import com.kg.gettransfer.domain.model.Payment
 
+import org.koin.standalone.get
+
 /**
  * Map a [PaymentEntity] to and from a [Payment] instance when data is moving between this later and the Domain layer.
  */
 open class PaymentMapper : Mapper<PaymentEntity, Payment> {
-    /**
-     * Map a [PaymentEntity] instance to a [Payment] instance.
-     */
+    private val paramsMapper = get<ParamsMapper>()
+
     override fun fromEntity(type: PaymentEntity) =
         Payment(
             type = type.type,
             url = type.url,
-            id = type.id
+            id = type.id,
+            params = type.params?.let { paramsMapper.fromEntity(it) }
         )
 
     override fun toEntity(type: Payment): PaymentEntity { throw UnsupportedOperationException() }
