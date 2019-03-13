9d909c979a9e91a10fbe135418fb6d6e0c5414d8
Evgeny <e.gromov@key-g.com>
2019-03-13 19:04:42 +0300

    gaa-656. added screen for paypal processing

---
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/presenter/PaypalConnectioPresenter.kt
@@ -0,0 +1,8 @@
+package com.kg.gettransfer.presentation.presenter
+
+import com.arellomobile.mvp.InjectViewState
+import com.kg.gettransfer.presentation.view.PaypalConnectionView
+
+@InjectViewState
+class PaypalConnectioPresenter: BasePresenter<PaypalConnectionView>() {
+}
