9e0b227f5d70f8d50235789467c8b3d4369af911
Evgeny <e.gromov@key-g.com>
2019-03-15 19:21:18 +0300

    gaa-656. setup navigation for paypal connection screen

--- data/src/main/kotlin/com/kg/gettransfer/data/model/PaymentEntity.kt
+++ data/src/main/kotlin/com/kg/gettransfer/data/model/PaymentEntity.kt
@@ -3,12 +3,24 @@ package com.kg.gettransfer.data.model
 data class PaymentEntity(
     val type: String,
     val url: String?,
-    val id: Long?
+    val id: Long?,
+    val params: ParamsEntity?
 ) {
 
     companion object {
         const val TYPE       = "type"
         const val URL        = "url"
         const val PAYMENT_ID = "payment_id"
+        const val PARAMS     = "params"
+    }
+}
+
+data class ParamsEntity(val amount: String,
+                        val currency: String,
+                        val paymentId: Long) {
+    companion object {
+        const val AMOUNT     = "amount"
+        const val CURRENCY   = "currency"
+        const val PAYMENT_ID    = "payment_id"
     }
 }
--- remote/src/main/kotlin/com/kg/gettransfer/remote/model/PaymentModel.kt
+++ remote/src/main/kotlin/com/kg/gettransfer/remote/model/PaymentModel.kt
@@ -4,9 +4,15 @@ import com.kg.gettransfer.data.model.PaymentEntity
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import com.kg.gettransfer.data.model.ParamsEntity
 
 data class PaymentModel(
     @SerializedName(PaymentEntity.TYPE) @Expose val type: String,
     @SerializedName(PaymentEntity.URL) @Expose val url: String?,
-    @SerializedName(PaymentEntity.PAYMENT_ID) @Expose val id: Long?
+    @SerializedName(PaymentEntity.PAYMENT_ID) @Expose val id: Long?,
+    @SerializedName(PaymentEntity.PARAMS) @Expose val params: Params?
 )
+
+data class Params(@SerializedName(ParamsEntity.AMOUNT) @Expose val amount: String,
+                  @SerializedName(ParamsEntity.CURRENCY) @Expose val currency: String,
+                  @SerializedName(ParamsEntity.PAYMENT_ID) @Expose val paymentId: Long)
