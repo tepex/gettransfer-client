ba0b466c31670879f45c7b42ab9b571fd506bee2
Evgeny <e.gromov@key-g.com>
2019-03-25 19:42:40 +0300

    gaa-866.handle facebook, viber, telegram click

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SupportActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SupportActivity.kt
@@ -1,5 +1,8 @@
 package com.kg.gettransfer.presentation.ui
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -26,6 +29,36 @@ class SupportActivity : BaseActivity(), SupportView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
         setupToolbar()
+        fabFacebook.setOnClickListener { facebookClick() }
+//        fabWhatsapp.setOnClickListener { whatsAppClick() }
+        fabViber.setOnClickListener { viberClick() }
+        fabTelegram.setOnClickListener { telegramClick() }
+    }
+
+    private fun facebookClick() {
+        var intent: Intent
+        try {
+            packageManager.getPackageInfo("com.facebook.katana", 0)
+            intent = Intent(Intent.ACTION_VIEW, Uri.parse("fb://facewebmodal/f?href=https://www.facebook.com/gettransfer"))
+        } catch (e: PackageManager.NameNotFoundException) {
+            intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/gettransfer"))
+        }
+        startActivity(intent)
+    }
+
+    private fun whatsAppClick() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(""))
+        startActivity(intent)
+    }
+
+    private fun viberClick() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://viber.me/GetTransferSupport"))
+        startActivity(intent)
+    }
+
+    private fun telegramClick() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/gettransfersupportbot"))
+        startActivity(intent)
     }
 
     private fun setupToolbar() {
