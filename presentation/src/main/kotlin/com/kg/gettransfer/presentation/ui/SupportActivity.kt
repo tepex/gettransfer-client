7d457f3e219746f096558837558df7bd07f6941e
Evgeny <e.gromov@key-g.com>
2019-03-25 18:41:10 +0300

    gaa-866.added layout for support screen

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SupportActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SupportActivity.kt
@@ -1,11 +1,16 @@
 package com.kg.gettransfer.presentation.ui
 
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.kg.gettransfer.R
 import com.kg.gettransfer.presentation.presenter.SupportPresenter
 import com.kg.gettransfer.presentation.view.SupportView
+import kotlinx.android.synthetic.main.activity_support.*
+import kotlinx.android.synthetic.main.toolbar_nav_back.*
+import kotlinx.android.synthetic.main.toolbar_nav_back.view.*
+import kotlinx.android.synthetic.main.view_contacts.view.*
 
 class SupportActivity : BaseActivity(), SupportView {
 
@@ -20,5 +25,13 @@ class SupportActivity : BaseActivity(), SupportView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar as Toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.ivBack.setOnClickListener { presenter.onBackCommandClick() }
+        toolbar.toolbar_title.text = getString(R.string.LNG_CUSTOMER_SUPPORT)
     }
 }
