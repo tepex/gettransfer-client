3154ae5c3b0b477307a8111b969975cdbdf8b11c
Evgeny <e.gromov@key-g.com>
2019-04-08 15:39:42 +0300

    gaa-1016. rounding rating

--- remote/src/main/kotlin/com/kg/gettransfer/remote/mapper/RatingsMapper.kt
+++ remote/src/main/kotlin/com/kg/gettransfer/remote/mapper/RatingsMapper.kt
@@ -3,6 +3,7 @@ package com.kg.gettransfer.remote.mapper
 import com.kg.gettransfer.data.model.RatingsEntity
 
 import com.kg.gettransfer.remote.model.RatingsModel
+import java.math.RoundingMode
 
 /**
  * Map a [RatingsModel] from an [RatingsEntity] instance when data is moving between this later and the Data layer.
@@ -10,7 +11,7 @@ import com.kg.gettransfer.remote.model.RatingsModel
 open class RatingsMapper : EntityMapper<RatingsModel, RatingsEntity> {
     override fun fromRemote(type: RatingsModel) =
         RatingsEntity(
-            average = type.average,
+            average = type.average?.toBigDecimal()?.setScale(1, RoundingMode.UP)?.toFloat(),
             vehicle = type.vehicle,
             driver = type.driver,
             fair = type.fair
