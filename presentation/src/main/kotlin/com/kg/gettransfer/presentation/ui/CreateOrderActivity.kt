fb14fded702a67d593c94dc2fa63a5fe83e42cf1
Evgeny <e.gromov@key-g.com>
2019-04-15 16:36:57 +0300

    gaa-964. fix after merging

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/CreateOrderActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/CreateOrderActivity.kt
@@ -292,7 +292,7 @@ class CreateOrderActivity : BaseGoogleMapActivity(), CreateOrderView, DateTimeSc
     }
 
     override fun setHintForDateTimeTransfer(withReturnWay: Boolean) {
-        transfer_date_time_field.field_input.hint = dateDelegate.getTextForMinDate(this)
+        transfer_date_time_field.field_input.setText(dateDelegate.getTextForMinDate(this))
     }
 
     private fun checkMinusButton(count: Int, minimum: Int, view: ImageView) {
