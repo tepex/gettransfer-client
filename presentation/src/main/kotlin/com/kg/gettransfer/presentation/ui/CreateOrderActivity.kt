84cec1bb04294826c036e62705cf60fcfcfc8b1a
Serj T <s.tarasov@key-g.com>
2019-04-23 19:02:56 +0300

    1051 banners

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/CreateOrderActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/CreateOrderActivity.kt
@@ -20,7 +20,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.text.InputFilter
 import android.text.InputType //don't delete
 import android.text.TextUtils //don't delete
-import android.util.Log
 
 import android.view.*
 import android.view.animation.AnimationUtils
