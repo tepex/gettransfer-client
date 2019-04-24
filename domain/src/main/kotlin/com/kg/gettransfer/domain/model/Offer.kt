1ed918e3ff38ef1ff7c958cc89c596aef1dbc54a
Serj T <s.tarasov@key-g.com>
2019-04-24 14:06:36 +0300

    banner, ratings

--- domain/src/main/kotlin/com/kg/gettransfer/domain/model/Offer.kt
+++ domain/src/main/kotlin/com/kg/gettransfer/domain/model/Offer.kt
@@ -29,8 +29,7 @@ data class Offer(
 
     fun isOfferRatedByUser() =                //call after check
         ratings?.run {
-     //       vehicle == NO_RATE && driver == NO_RATE && fair == NO_RATE
-            vehicle != null && driver != NO_RATE && fair != NO_RATE
+            vehicle != NO_RATE && driver != NO_RATE && fair != NO_RATE
         } ?: true
 
     companion object {
