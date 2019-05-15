abd633ea2e9d0d0cb447075553a1cefb36af0e18
Serj T <s.tarasov@key-g.com>
2019-05-15 15:55:45 +0300

    GAA 1218, 1220

--- presentation/src/main/kotlin/com/kg/gettransfer/extensions/PhoneNumberUtilExtension.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/extensions/PhoneNumberUtilExtension.kt
@@ -2,7 +2,18 @@ package com.kg.gettransfer.extensions
 
 import com.kg.gettransfer.presentation.ui.Utils
 import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
+import io.michaelrocks.libphonenumber.android.Phonenumber
+import java.util.*
 
-fun PhoneNumberUtil.internationalExample(country: String): String =
-        Utils.phoneUtil.format(Utils.phoneUtil.getExampleNumber(country),
+fun PhoneNumberUtil.internationalExample(country: Locale): String =
+        Utils.phoneUtil.format(getNumber(country),
                 PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL)
+
+fun getNumber(country: Locale): Phonenumber.PhoneNumber =
+        Utils.phoneUtil.getExampleNumber(country.country)
+                ?: Utils.phoneUtil.getExampleNumberForNonGeoEntity(
+                        getCountryCode(country)
+                )
+
+fun getCountryCode(locale: Locale) =
+        Utils.phoneUtil.getCountryCodeForRegion(locale.country)
