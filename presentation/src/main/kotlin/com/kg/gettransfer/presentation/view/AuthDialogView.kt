5857ea8dc7f151bae8ec87e450ebdeeba89f06d4
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2019-05-22 17:10:32 +0300

    Deleted AuthDialogFragemnt

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/AuthDialogView.kt
+++
@@ -1,8 +0,0 @@
-package com.kg.gettransfer.presentation.view
-
-interface AuthDialogView: BaseView {
-    fun setEmail(email: String, withFocus: Boolean)
-    fun setPhone(phone: String, withFocus: Boolean)
-    fun redirectToLogin()
-    fun onAccountCreated()
-}
