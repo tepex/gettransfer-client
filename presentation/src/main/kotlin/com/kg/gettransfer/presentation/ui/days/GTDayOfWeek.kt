ddfef6cb02aa77d9c31c596b5aa68acd72f7d096
Viacheslav Andrushkevich <andrushkva17@mail.ru>
2019-05-28 16:59:32 +0300

    Small refactoring carrier days of week

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/days/GTDayOfWeek.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/days/GTDayOfWeek.kt
@@ -2,7 +2,7 @@ package com.kg.gettransfer.presentation.ui.days
 
 abstract class GTDayOfWeek {
     abstract val day: Int
-    abstract fun getOffset(): Int
+    abstract val offset: Int
 
     companion object {
         fun getWeekDays() =
@@ -12,29 +12,29 @@ abstract class GTDayOfWeek {
 
 class Monday: GTDayOfWeek() {
     override val day = 1
-    override fun getOffset() = -1
+    override val offset = -1
 }
 class Tuesday: GTDayOfWeek() {
     override val day = 2
-    override fun getOffset() = -2
+    override val offset = -2
 }
 class Wednesday: GTDayOfWeek() {
     override val day = 3
-    override fun getOffset() = -3
+    override val offset = -3
 }
 class Thursday: GTDayOfWeek() {
     override val day = 4
-    override fun getOffset() = +3
+    override val offset = +3
 }
 class Friday: GTDayOfWeek() {
     override val day = 5
-    override fun getOffset() = +2
+    override val offset = +2
 }
 class Saturday: GTDayOfWeek() {
     override val day = 6
-    override fun getOffset() = +1
+    override val offset = +1
 }
 class Sunday: GTDayOfWeek() {
     override val day = 7
-    override fun getOffset() = 0
+    override val offset = 0
 }
