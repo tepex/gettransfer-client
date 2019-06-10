57c1f5033f5789ca45a3c19e1c058c6a9d87df31
Tepex <k.terehov@key-g.com>
2019-06-10 14:19:43 +0300

    style: split imports by groups

--- presentation/src/main/kotlin/com/kg/gettransfer/GTApplication.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/GTApplication.kt
@@ -2,41 +2,36 @@ package com.kg.gettransfer
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.ProcessLifecycleOwner
-
-import android.content.Intent
-
 import android.os.Build
 
 import android.support.annotation.CallSuper
 import android.support.multidex.MultiDexApplication
+
 import com.appsflyer.AppsFlyerConversionListener
 import com.appsflyer.AppsFlyerLib
 
-import com.google.android.gms.tasks.OnCompleteListener
-
 import com.google.firebase.FirebaseApp
-import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 
 import com.kg.gettransfer.cache.cacheModule
 import com.kg.gettransfer.data.dataModule
 import com.kg.gettransfer.di.*
 import com.kg.gettransfer.presentation.FileLoggingTree
+
 import com.kg.gettransfer.remote.remoteMappersModule
 import com.kg.gettransfer.remote.remoteModule
 import com.kg.gettransfer.remote.socketModule
+
 import com.kg.gettransfer.utilities.AppLifeCycleObserver
 
 import com.squareup.leakcanary.LeakCanary
 
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
+
 import io.sentry.Sentry
 import io.sentry.android.AndroidSentryClientFactory
-
-import net.hockeyapp.android.CrashManager
 
 import org.koin.android.ext.android.startKoin
 
