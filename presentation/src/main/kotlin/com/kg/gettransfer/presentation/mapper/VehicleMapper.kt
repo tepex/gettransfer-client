156ea7a5e273a85a3bdbd6aad029198bcd54e84b
Evgeny <e.gromov@key-g.com>
2019-06-17 18:16:03 +0300

    fix: trivial
    gaa-1422

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/mapper/VehicleMapper.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/mapper/VehicleMapper.kt
@@ -1,6 +1,5 @@
 package com.kg.gettransfer.presentation.mapper
 
-import com.kg.gettransfer.domain.interactor.SystemInteractor
 import com.kg.gettransfer.domain.model.Vehicle
 
 import com.kg.gettransfer.presentation.model.VehicleModel
@@ -8,9 +7,8 @@ import com.kg.gettransfer.presentation.model.VehicleModel
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.get
 
-open class VehicleMapper() : KoinComponent {
+open class VehicleMapper : KoinComponent {
     private val transportTypeMapper = get<TransportTypeMapper>()
-    private val systemInteractor = get<SystemInteractor>()
 
     fun toView(type: Vehicle) =
         VehicleModel(
@@ -20,6 +18,6 @@ open class VehicleMapper() : KoinComponent {
             year               = type.year,
             color              = type.color,
             transportType      = transportTypeMapper.toView(type.transportType),
-            photos             = type.photos.map { photo -> "${systemInteractor.endpoint.url}$photo" }
+            photos             = type.photos
         )
 }
