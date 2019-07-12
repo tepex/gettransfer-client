cdadf91b74cd01a25a1ce02b58878ec9e87cde9a
anastasia.p <anastasiapleshkan@gmail.com>
2019-07-12 12:44:28 +0300

    fix: IllegalFormatConversionException in java.util.Formatter.badArgumentType, line 1489
    fixes: GAA-1537

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/helpers/DateTimePickerHelper.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/helpers/DateTimePickerHelper.kt
@@ -3,7 +3,10 @@ package com.kg.gettransfer.presentation.ui.helpers
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Resources
 import android.os.Build
+import android.support.annotation.NonNull
 import com.kg.gettransfer.common.BoundDatePickerDialog
 import com.kg.gettransfer.common.BoundTimePickerDialog
 import org.koin.core.KoinComponent
@@ -11,9 +14,53 @@ import java.util.*
 
 object DateTimePickerHelper : KoinComponent {
 
+    /**
+     * It's a Samsung bug in their Lollipop UI implementation. It affects Galaxy S4, S5, Note 3 and probably more devices.
+     * For us it occurs on de_DE and de_AT languages, but it appears to be an issue that affects multiple languages.
+     *
+     * I fixed it by forcing Samsung devices to use the Holo theme for the date picker.
+     */
+    private fun isBrokenSamsungDevice(): Boolean {
+        return Build.MANUFACTURER.equals("samsung", ignoreCase = true) && isBetweenAndroidVersions(
+                Build.VERSION_CODES.LOLLIPOP,
+                Build.VERSION_CODES.LOLLIPOP_MR1)
+    }
+
+    private fun isBetweenAndroidVersions(min: Int, max: Int): Boolean {
+        return Build.VERSION.SDK_INT in min..max
+    }
+
+    private fun getContextForDateTimePicker(context: Context) : Context {
+        if (isBrokenSamsungDevice())
+            return object : ContextWrapper(context) {
+                private var wrappedResources: Resources? = null
+                override fun getResources(): Resources? {
+                    val r = super.getResources()
+                    if (wrappedResources == null) {
+                        wrappedResources = object : Resources(r.assets, r.displayMetrics, r.configuration) {
+                            @NonNull
+                            @Throws(NotFoundException::class)
+                            override fun getString(id: Int, vararg formatArgs: Any): String {
+                                try {
+                                    return super.getString(id, formatArgs)
+                                } catch (ifce: IllegalFormatConversionException) {
+                                    var template = super.getString(id)
+                                    template = template.replace(("%" + ifce.conversion).toRegex(), "%s")
+                                    return String.format(configuration.locale, template, formatArgs)
+                                }
+                            }
+                        }
+                    }
+                    return wrappedResources
+                }
+            }
+        return context
+    }
+
     fun showDatePickerDialog(context: Context, selectedDate: Calendar, current: Calendar, handler: DateTimeHandler) {
         val calendar: Calendar = selectedDate
-        val boundDatePickerDialog = BoundDatePickerDialog(context,
+        val ctx = getContextForDateTimePicker(context)
+        val boundDatePickerDialog = BoundDatePickerDialog(ctx,
             calendar.get(Calendar.YEAR),
             calendar.get(Calendar.MONTH),
             calendar.get(Calendar.DAY_OF_MONTH),
@@ -24,7 +71,7 @@ object DateTimePickerHelper : KoinComponent {
                 calendarWithoutTime.set(year, monthOfYear, dayOfMonth, 0, 0)
                 when {
                     calendarWithoutTime.time.after(current.time) -> showTimePickerDialog(
-                        context,
+                        ctx,
                         calendar,
                         -1,
                         24,
@@ -35,7 +82,7 @@ object DateTimePickerHelper : KoinComponent {
 
                     calendar.time.after(current.time) ->
                         showTimePickerDialog(
-                            context,
+                            ctx,
                             calendar,
                             current.get(Calendar.HOUR_OF_DAY),
                             current.get(Calendar.MINUTE),
@@ -45,7 +92,7 @@ object DateTimePickerHelper : KoinComponent {
                         )
 
                     else -> showTimePickerDialog(
-                        context,
+                        ctx,
                         calendar,
                         current.get(Calendar.HOUR_OF_DAY),
                         current.get(Calendar.MINUTE),
@@ -79,8 +126,9 @@ object DateTimePickerHelper : KoinComponent {
         setMinute: Int,
         handler: DateTimeHandler
     ) {
+        val ctx = getContextForDateTimePicker(context)
         BoundTimePickerDialog(
-            context,
+            ctx,
             @Suppress("DEPRECATION") AlertDialog.THEME_HOLO_LIGHT,
             { _, hour, minute ->
                 selectedDate.set(Calendar.HOUR_OF_DAY, hour)
