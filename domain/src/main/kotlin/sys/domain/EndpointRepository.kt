59a6a546f4268d3ff5146830300b1023d538c1c1
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2019-08-13 12:58:16 +0300

    fix: changing endpoint bug

--- domain/src/main/kotlin/sys/domain/EndpointRepository.kt
+++ domain/src/main/kotlin/sys/domain/EndpointRepository.kt
@@ -2,4 +2,4 @@ package com.kg.gettransfer.sys.domain
 
 import com.kg.gettransfer.core.domain.WriteableRepository
 
-interface EndpointRepository : WriteableRepository<String>
+interface EndpointRepository : WriteableRepository<Endpoint>
