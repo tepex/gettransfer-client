bb77bd7713fcd9cb1ed3c3341716353b2c9f1cba
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2019-08-14 15:46:17 +0300

    fix: buttons position in payment successful screen & added button chat in this screen
    GAA-1716

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/PaymentSuccessfulView.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/PaymentSuccessfulView.kt
@@ -3,11 +3,11 @@ package com.kg.gettransfer.presentation.view
 import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.model.LatLng
 import com.kg.gettransfer.presentation.model.PolylineModel
-import java.io.InputStream
 
 interface PaymentSuccessfulView: BaseView {
     fun setRoute(polyline: PolylineModel)
     fun setRemainTime(days: Int, hours: Int, minutes: Int)
     fun setPinHourlyTransfer(point: LatLng, cameraUpdate: CameraUpdate)
     fun initCallButton()
+    fun initChatButton()
 }
