2d741a9c9f2d22597cf24682ab43b5e06e257735
anastasia.p <anastasiapleshkan@gmail.com>
2019-08-16 16:06:05 +0300

    ui: improvement design bottom navigation

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/MainNavigateActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/MainNavigateActivity.kt
@@ -142,10 +142,12 @@ class MainNavigateActivity : BaseActivity(), MainNavigateView,
             RequestsPagerFragment::class.java.name,
             SettingsFragment::class.java.name,
             SupportFragment::class.java.name -> {
+                bottom_nav_shadow.visibleAnimation(true, parent_container)
                 bottom_nav.visibleAnimation(true, parent_container)
             }
             //not visible bottom menu
             else -> {
+                bottom_nav_shadow.visibleAnimation(false, parent_container)
                 bottom_nav.visibleAnimation(false, parent_container)
             }
         }
