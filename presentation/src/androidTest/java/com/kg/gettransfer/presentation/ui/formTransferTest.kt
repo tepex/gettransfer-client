aadb51a17001d7a0979608d978cda57eb22f587a
Roza Abdullina <r.abdullina@gettransfer.com>
2019-08-27 18:08:13 +0300

    add authorization user

--- presentation/src/androidTest/java/com/kg/gettransfer/presentation/ui/formTransferTest.kt
+++ presentation/src/androidTest/java/com/kg/gettransfer/presentation/ui/formTransferTest.kt
@@ -1,8 +1,11 @@
 package com.kg.gettransfer.presentation.ui
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
@@ -12,8 +15,11 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kg.gettransfer.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -22,24 +28,45 @@ import org.junit.runner.RunWith
 class formTransferTest{
     @Test
     fun inputField (){
-        Thread.sleep(7000)
-        ActivityScenario.launch(SplashActivity::class.java)
-        Thread.sleep(7000)
-        onView(Matchers.allOf(ViewMatchers.withId(R.id.nav_settings))).perform(ViewActions.click())
-        Thread.sleep(7000)
-//        onView(withId(R.id.settingsProfile))
-//                .check(matches(isDisplayed()))
-//fieldText
-        onView(Matchers.allOf(ViewMatchers.withId(R.id.settingsProfile))).perform(ViewActions.click())
-        Thread.sleep(7000)
-//        onView(allOf(withText(R.string.LNG_LOGIN_EMAIL_SECTION))).perform(typeText("r.abdullina@gettransfer.com"))
-//        onView(allOf(withId(R.id.fieldText))).perform(typeText("r.abdullina@gettransfer.com"))
 
-//        onView(allOf(withId(R.id.fieldText), isDisplayed())).perform(click())
-//        onView(allOf(withId(R.id.loginTitleText)),isDescendantOfA(withId(R.id.linear_layout))).perform(typeText("r.abdullina@gettransfer.com"))
+        Thread.sleep(7000)
+
+        ActivityScenario.launch(SplashActivity::class.java)
+
+        Thread.sleep(7000)
+
+        onView(Matchers.allOf(ViewMatchers.withId(R.id.nav_settings))).perform(ViewActions.click())
+        onView(Matchers.allOf(ViewMatchers.withId(R.id.settingsProfile))).perform(ViewActions.click())
+
+        val appCompatEditText2 = onView(
+                allOf(withId(R.id.fieldText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.fieldLayout),
+                                        0),
+                                0),
+                        isDisplayed()))
+        appCompatEditText2.perform(ViewActions.replaceText("mygtracc1@gmail.com"), ViewActions.closeSoftKeyboard())
 
         onView(allOf(withId(R.id.etPassword))).perform(typeText("PassRR11"))
+        onView(Matchers.allOf(ViewMatchers.withId(R.id.btnLogin))).perform(ViewActions.click())
 
         Thread.sleep(7000)
+    }
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
     }
 }
