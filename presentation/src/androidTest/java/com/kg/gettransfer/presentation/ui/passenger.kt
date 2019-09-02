3d57e51e54b1286fb6417335e62c5875e3c1ec1f
Roza Abdullina <r.abdullina@gettransfer.com>
2019-09-02 19:46:47 +0300

    GAA-1761: refactor code

--- presentation/src/androidTest/java/com/kg/gettransfer/presentation/ui/passenger.kt
+++ presentation/src/androidTest/java/com/kg/gettransfer/presentation/ui/passenger.kt
@@ -29,15 +29,16 @@ import org.junit.runner.RunWith
 import androidx.test.espresso.contrib.PickerActions
 import com.kg.gettransfer.presentation.ui.helpers.DateTimePickerHelper
 import kotlinx.android.synthetic.main.search_address.view.*
+import org.hamcrest.Matchers.endsWith
 
 //import android.R
 
 
 
 @RunWith(AndroidJUnit4::class)
-class formTransferTest{
+class passenger {
     @Test
-    fun inputField (){
+    fun passengerCreateTrasfer (){
 
         Thread.sleep(7000)
 
@@ -55,115 +56,72 @@ class formTransferTest{
         //Assertion user exist
         profileOnboardingAnyway()
 
-        Thread.sleep(7000)
+        Thread.sleep(700)
 
         onView(Matchers.allOf(ViewMatchers.withId(R.id.sub_title),withText("Pickup location"),isDisplayed())).perform(ViewActions.click())
 
-        Thread.sleep(7000)
+        Thread.sleep(700)
+        val placeFrom = onView(allOf(withId(R.id.addressField), isDescendantOfA(withId(R.id.searchFrom))))
+        Thread.sleep(700)
+        placeFrom.perform(ViewActions.replaceText("Moscow"), ViewActions.closeSoftKeyboard())
+        Thread.sleep(700)
+        val mskAddressItem =   onView(allOf(withId(R.id.addressItem),withText("Moscow, Russia"),isDisplayed()))
+        Thread.sleep(700)
+        mskAddressItem.perform(click())
 
-        val appCompatEditText3 = onView(
-                allOf(withId(R.id.addressField),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.searchFrom),
-                                        0),
-                                1),
-                        isDisplayed()))
-        appCompatEditText3.perform(click()).perform(ViewActions.replaceText("Moscow"), ViewActions.closeSoftKeyboard())
+        Thread.sleep(700)
+        val placeTo = onView(allOf(withId(R.id.addressField), isDescendantOfA(withId(R.id.searchTo))))
+        Thread.sleep(700)
+        placeTo.perform(ViewActions.replaceText("Saint Petersburg"), ViewActions.closeSoftKeyboard())
+        Thread.sleep(2000)
+        val spbAddressItem =   onView(allOf(withId(R.id.addressItem),withText("Saint Petersburg, Russia"),isDisplayed()))
+        Thread.sleep(2000)
+        spbAddressItem.perform(click())
+        Thread.sleep(2000)
 
-        Thread.sleep(7000)
-        val linearLayout = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.rv_addressList),
-                                childAtPosition(
-                                        withClassName(Matchers.`is`("android.widget.LinearLayout")),
-                                        1)),
-                        0),
-                        isDisplayed()))
-        linearLayout.perform(click())
-
-        val appCompatEditText4 = onView(
-                allOf(withId(R.id.addressField),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.searchTo),
-                                        0),
-                                1),
-                        isDisplayed()))
-        appCompatEditText4
-                .perform(click())
-                .perform(ViewActions.replaceText("Saint Petersburg"))
-        Thread.sleep(7000)
-        val linearLayout2 = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.rv_addressList),
-                                childAtPosition(
-                                        withClassName(Matchers.`is`("android.widget.LinearLayout")),
-                                        1)),
-                        0),
-                        isDisplayed()))
-        linearLayout2.perform(click())
-
-        Thread.sleep(7000)
         onView(withId(R.id.rvTransferType)).perform(ViewActions.swipeUp())
-        Thread.sleep(500)
+        Thread.sleep(700)
 
-//Select date
+        //Select date
         onView(withId(R.id.transfer_date_time_field)).perform(ViewActions.click())
-    onView(withClassName(Matchers.equalTo(DatePicker::class.java.name)))
+        onView(withClassName(Matchers.equalTo(DatePicker::class.java.name)))
         .perform(PickerActions.setDate(2019, 9,15))
-        Thread.sleep(7000)
-
-        val appCompatButton = onView(
-                allOf(withId(android.R.id.button1), withText("OK"),
-                        childAtPosition(
-                                allOf(withClassName(Matchers.`is`("com.android.internal.widget.ButtonBarLayout")),
-                                        childAtPosition(
-                                                withClassName(Matchers.`is`("android.widget.LinearLayout")),
-                                                3)),
-                                3),
-                        isDisplayed()))
-        appCompatButton.perform(click())
-        Thread.sleep(7000)
+        Thread.sleep(700)
+        val okButton = onView(withId(android.R.id.button1))
+        okButton.perform(click())
+        Thread.sleep(700)
 // The end code select date
 // Select time
         onView(withClassName(Matchers.equalTo(TimePicker::class.java.name)))
                 .perform(PickerActions.setTime(15,15))
-        Thread.sleep(5000)
-        val appCompatButton2 = onView(
-                allOf(withId(android.R.id.button1), withText("OK"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(Matchers.`is`("android.widget.LinearLayout")),
-                                        0),
-                                2),
-                        isDisplayed()))
-        appCompatButton2.perform(click())
-        Thread.sleep(2000)
+        Thread.sleep(700)
+        okButton.perform(click())
+        Thread.sleep(700)
 // The end code select time
 
         onView(withId(R.id.img_plus_seat)).perform(ViewActions.click())
-        Thread.sleep(2000)
+        Thread.sleep(700)
 
         onView(withId(R.id.btnGetOffers)).perform(ViewActions.click())
 
         //Assertion dialog-window exist
         carOnboardingAnyway ()
         switchOnboardingAnyway()
-        Thread.sleep(7000)
+        Thread.sleep(700)
 
         val cardOne = onView(withIndex(withId(R.id.tv_car_model_tiny),0))
         val cardTwo = onView(withIndex(withId(R.id.tv_car_model_tiny),1))
         val cardThree = onView(withIndex(withId(R.id.tv_car_model_tiny),2))
-        Thread.sleep(500)
+        Thread.sleep(700)
         cardOne.perform(click());
-        Thread.sleep(7000)
+        Thread.sleep(700)
         onView(withId(R.id.btn_book)).perform(ViewActions.click())
-        Thread.sleep(7000)
+        Thread.sleep(700)
         onView(withId(R.id.btnBack)).perform(ViewActions.click())
-        Thread.sleep(7000)
+        Thread.sleep(700)
 
     }
+
     fun withIndex(matcher: Matcher<View>, index: Int): Matcher<View> {
         return object : TypeSafeMatcher<View>() {
             internal var currentIndex = 0
@@ -179,6 +137,7 @@ class formTransferTest{
             }
         }
     }
+
     fun ViewInteraction.isDisplayed(): Boolean {
         try {
             check(matches(ViewMatchers.isDisplayed()))
@@ -207,20 +166,16 @@ class formTransferTest{
             onView(Matchers.allOf(ViewMatchers.withId(R.id.nav_order))).perform(ViewActions.click())
 
         } else {
-            val appCompatEditText2 = onView(
-                    allOf(withId(R.id.fieldText),
-                            childAtPosition(
-                                    childAtPosition(
-                                            withId(R.id.fieldLayout),
-                                            0),
-                                    0),
-                            isDisplayed()))
-            appCompatEditText2.perform(ViewActions.replaceText("mygtracc1@gmail.com"), ViewActions.closeSoftKeyboard())
 
+            val email = onView(allOf(withId(R.id.fieldText), isDescendantOfA(withId(R.id.fieldLayout)), isDisplayed()))
+
+            email.perform(ViewActions.replaceText("mygtracc1@gmail.com"), ViewActions.closeSoftKeyboard())
+
+            Thread.sleep(700)
             onView(allOf(withId(R.id.etPassword))).perform(typeText("PassRR11"))
             onView(Matchers.allOf(ViewMatchers.withId(R.id.btnLogin))).perform(ViewActions.click())
 
-            Thread.sleep(7000)
+            Thread.sleep(700)
 
             onView(Matchers.allOf(ViewMatchers.withId(R.id.nav_order))).perform(ViewActions.click())
         }
@@ -229,47 +184,33 @@ class formTransferTest{
     fun carOnboardingAnyway () {
         if(onView(withText("Please select transport type")).isDisplayed()) {
             onView(Matchers.allOf(ViewMatchers.withId(android.R.id.button1), withText("OK"))).perform(ViewActions.click())
-            Thread.sleep(7000)
+            Thread.sleep(700)
             //Select car
             onView(withId(R.id.rvTransferType)).perform (ViewActions.click())
-            Thread.sleep(7000)
+            Thread.sleep(700)
             //Click btn
             onView(withId(R.id.btnGetOffers)).perform (ViewActions.click())
-            Thread.sleep(7000)
+            Thread.sleep(700)
 
         }  else {
 
-        }}
+        }
+    }
        fun switchOnboardingAnyway() {
            if (onView(withText("You should accept terms of use")).isDisplayed()) {
             onView(Matchers.allOf(ViewMatchers.withId(android.R.id.button1), withText("OK"))).perform(ViewActions.click())
-            Thread.sleep(7000)
+            Thread.sleep(700)
             onView(withId(R.id.scrollContent)).perform(ViewActions.swipeUp())
-            Thread.sleep(2000)
+            Thread.sleep(700)
             //Activate switcher
             onView(withId(R.id.switchAgreement)).perform(ViewActions.click())
-            Thread.sleep(7000)
+            Thread.sleep(700)
             onView(withId(R.id.btnGetOffers)).perform(ViewActions.click())
         } else {
 
         }
     }
 
-    private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
 
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
-    }
 
 }
