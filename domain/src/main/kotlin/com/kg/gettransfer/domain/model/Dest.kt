95b802ffc4e754d1cafafa58158383707ccc4910
Gromov Evgeny <e.gromov@key-g.com>
2019-09-24 17:43:34 +0300

    fix: disable null checks(!!) for type of transfer. show error if transfer is null
    GAA-1636

--- domain/src/main/kotlin/com/kg/gettransfer/domain/model/Dest.kt
+++ domain/src/main/kotlin/com/kg/gettransfer/domain/model/Dest.kt
@@ -1,6 +1,7 @@
 package com.kg.gettransfer.domain.model
 
-sealed class Dest<CityPoint, Int>
+sealed class Dest<CityPoint, Int> {
 
-class DestPoint<CityPoint, Int>(val to: CityPoint) : Dest<CityPoint, Int>()
-class DestDuration<CityPoint, Int>(val duration: Int) : Dest<CityPoint, Int>()
+    class Point(val to: CityPoint) : Dest<CityPoint, Int>()
+    class Duration(val duration: Int) : Dest<CityPoint, Int>()
+}
