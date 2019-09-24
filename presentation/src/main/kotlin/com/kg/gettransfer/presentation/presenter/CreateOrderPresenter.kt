4818f9a93c29b9e34094610fb47d960a1e245a57
Gromov Evgeny <e.gromov@key-g.com>
2019-09-24 17:49:06 +0300

    fix: delete redundant let
    GAA-1636

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/presenter/CreateOrderPresenter.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/presenter/CreateOrderPresenter.kt
@@ -435,7 +435,7 @@ class CreateOrderPresenter : BasePresenter<CreateOrderView>() {
 
         @Suppress("UnsafeCallOnNullableType")
         val transferNew = toPoint?.let { dest ->
-            orderInteractor.from?.let { it.cityPoint }?.let { from ->
+            orderInteractor.from?.cityPoint?.let { from ->
                 transportTypes?.filter { it.checked }?.map { it.id }?.let { transportTypes ->
                     TransferNew(
                             from,
