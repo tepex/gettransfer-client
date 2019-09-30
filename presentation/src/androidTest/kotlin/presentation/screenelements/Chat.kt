37a651e424296566f0636240fec94aeae08231f1
Ivan Marchenkov <i.marchenkov@gettransfer.com>
2019-09-30 15:50:24 +0300

    ui-tests: create new regress tests GAA-1861

---
+++ presentation/src/androidTest/kotlin/presentation/screenelements/Chat.kt
@@ -0,0 +1,12 @@
+package presentation.screenelements
+
+import com.agoda.kakao.edit.KEditText
+import com.agoda.kakao.screen.Screen
+import com.agoda.kakao.text.KButton
+import com.kg.gettransfer.R
+
+object Chat : Screen<Chat>() {
+
+    val message  =  KEditText { withId(R.id.messageText) }
+    val Send  = KButton { withId(R.id.btnSend) }
+}
