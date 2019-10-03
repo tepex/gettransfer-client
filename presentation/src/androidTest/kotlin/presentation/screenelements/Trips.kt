e9a34995f4717c87684161f8bfa1e03b437d0f43
Ivan Marchenkov <i.marchenkov@gettransfer.com>
2019-10-03 15:50:48 +0300

    ui-tests: hot-fix GAA-1914

--- presentation/src/androidTest/kotlin/presentation/screenelements/Trips.kt
+++ presentation/src/androidTest/kotlin/presentation/screenelements/Trips.kt
@@ -4,21 +4,21 @@ import com.agoda.kakao.common.views.KSwipeView
 import com.agoda.kakao.common.views.KView
 import com.agoda.kakao.screen.Screen
 import com.agoda.kakao.text.KButton
-import com.agoda.kakao.text.KTextView
+
 import com.kg.gettransfer.R
 
 object Trips : Screen<Trips>() {
 
-    val Requests  = KView { withId(R.id.vpRequests) }
-    val swRequests  = KSwipeView { withId(R.id.vpRequests) }
+    val Requests = KView { withId(R.id.vpRequests) }
+    val swRequests = KSwipeView { withId(R.id.vpRequests) }
     val transferDetails = KSwipeView { withId(R.id.transfer_details_header) }
     val bookNowInfo = KSwipeView { withId(R.id.tv_bookNow_info) }
-    val transfer_details_main  = KView { withId(R.id.transfer_details_main) }
-    val bottomCommunicationButtons  = KView { withId(R.id.bottomCommunicationButtons) }
+    val transfer_details_main = KView { withId(R.id.transfer_details_main) }
+    val bottomCommunicationButtons = KView { withId(R.id.bottomCommunicationButtons) }
     val TransferTime = KSwipeView { withId(R.id.tvTransferTime) }
     val transfer = KSwipeView { withId(R.id.transfer_details_main) }
-    val flexboxTransportTypes  = KView { withId(R.id.flexboxTransportTypes) }
-    val topCommunicationButtons  = KView { withId(R.id.topCommunicationButtons) }
+    val flexboxTransportTypes = KView { withId(R.id.flexboxTransportTypes) }
+    val topCommunicationButtons = KView { withId(R.id.topCommunicationButtons) }
     val Cancel = KButton {
         withId(R.id.btnName)
         withText("Cancel\nrequest")
