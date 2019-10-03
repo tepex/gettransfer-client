ba4c258e0a120183ac13e615ffa6fd2a0ad63f3a
Ivan Marchenkov <i.marchenkov@gettransfer.com>
2019-10-03 15:42:50 +0300

    ui-tests: hot-fix GAA-1861

--- presentation/src/androidTest/kotlin/presentation/screenelements/Chat.kt
+++ presentation/src/androidTest/kotlin/presentation/screenelements/Chat.kt
@@ -3,10 +3,11 @@ package presentation.screenelements
 import com.agoda.kakao.edit.KEditText
 import com.agoda.kakao.screen.Screen
 import com.agoda.kakao.text.KButton
+
 import com.kg.gettransfer.R
 
 object Chat : Screen<Chat>() {
 
-    val message  =  KEditText { withId(R.id.messageText) }
-    val Send  = KButton { withId(R.id.btnSend) }
+    val message = KEditText { withId(R.id.messageText) }
+    val Send = KButton { withId(R.id.btnSend) }
 }
