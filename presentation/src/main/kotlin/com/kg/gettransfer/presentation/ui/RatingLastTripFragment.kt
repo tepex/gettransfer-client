4a8c294030944652aebac47adfd0721b2e464c1c
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2019-10-09 12:56:48 +0300

    fix: crash after showing dialog for rate transfer; optimized showing route on map in this dialog
    GAA-1947

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/RatingLastTripFragment.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/RatingLastTripFragment.kt
@@ -6,26 +6,13 @@ import androidx.annotation.CallSuper
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 
-import com.google.android.gms.maps.CameraUpdate
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-
 import com.kg.gettransfer.R
-import com.kg.gettransfer.domain.ApiException
-import com.kg.gettransfer.domain.DatabaseException
-
-import com.kg.gettransfer.presentation.model.RouteModel
-import com.kg.gettransfer.presentation.model.TransferModel
+import com.kg.gettransfer.domain.model.Transfer
 
 import com.kg.gettransfer.presentation.presenter.RatingLastTripPresenter
 
-import com.kg.gettransfer.presentation.ui.dialogs.BaseBottomSheetDialogFragment
+import com.kg.gettransfer.presentation.ui.dialogs.BaseMapBottomSheetDialogFragment
 import com.kg.gettransfer.presentation.ui.dialogs.RatingDetailDialogFragment
-import com.kg.gettransfer.presentation.ui.helpers.MapHelper
 
 import com.kg.gettransfer.presentation.view.BaseView
 import com.kg.gettransfer.presentation.view.RatingLastTripView
@@ -33,17 +20,17 @@ import com.kg.gettransfer.presentation.view.RatingLastTripView
 import kotlinx.android.synthetic.main.view_last_trip_rate.*
 
 @Suppress("TooManyFunctions")
-class RatingLastTripFragment : BaseBottomSheetDialogFragment(), RatingLastTripView, OnMapReadyCallback {
-
-    private lateinit var googleMap: GoogleMap
+class RatingLastTripFragment : BaseMapBottomSheetDialogFragment(), RatingLastTripView {
 
     override val layout: Int = R.layout.view_last_trip_rate
 
     @InjectPresenter
-    lateinit var presenter: RatingLastTripPresenter
+    internal lateinit var presenter: RatingLastTripPresenter
 
     @ProvidePresenter
     fun providePresenter() = RatingLastTripPresenter()
+
+    override fun getPresenter(): RatingLastTripPresenter = presenter
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,60 +51,15 @@ class RatingLastTripFragment : BaseBottomSheetDialogFragment(), RatingLastTripVi
     @CallSuper
     override fun initUi(savedInstanceState: Bundle?) {
         super.initUi(savedInstanceState)
-        val fragment = fragmentManager?.findFragmentById(R.id.rate_map)
-        if (fragment != null && fragment is SupportMapFragment) {
-            fragment.getMapAsync(this)
-        }
+        initMapFragment(R.id.rate_map)
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        googleMap = map
-        customizeGoogleMaps(googleMap)
-    }
-
-    private fun customizeGoogleMaps(gm: GoogleMap) {
-        gm.uiSettings.isRotateGesturesEnabled = false
-        gm.uiSettings.isTiltGesturesEnabled = false
-        gm.uiSettings.isMyLocationButtonEnabled = false
-        gm.uiSettings.isScrollGesturesEnabled = false
-        gm.uiSettings.isZoomGesturesEnabled = false
-    }
-
-    override fun setupReviewForLastTrip(transfer: TransferModel, startPoint: LatLng, routeModel: RouteModel?) {
+    override fun setupReviewForLastTrip(transfer: Transfer) {
         tv_transfer_number_rate.apply { text = text.toString().plus(" #${transfer.id}") }
-        tv_transfer_date_rate.text = SystemUtils.formatDateTime(transfer.dateTime)
+        tv_transfer_date_rate.text = SystemUtils.formatDateTime(transfer.dateToLocal)
         tv_vehicle_model_rate.text = arguments?.getString(EXTRA_VEHICLE)
         val color = arguments?.getString(EXTRA_COLOR) ?: ""
         context?.let { carColor_rate.setImageDrawable(Utils.getCarColorFormRes(it, color)) }
-        drawMapForReview(routeModel, transfer.from, startPoint)
-    }
-
-    private fun drawMapForReview(routeModel: RouteModel?, from: String, startPoint: LatLng) {
-        if (routeModel != null) {
-            val polyline = Utils.getPolyline(routeModel)
-            if (MapHelper.isEmptyPolyline(polyline, routeModel)) {
-                return
-            } else {
-                context?.let { MapHelper.setPolyline(it, layoutInflater, googleMap, polyline, routeModel) }
-            }
-        } else {
-            setPinForHourlyTransfer(from, "", startPoint, Utils.getCameraUpdateForPin(startPoint))
-        }
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    private fun setPinForHourlyTransfer(
-        placeName: String,
-        info: String,
-        point: LatLng,
-        cameraUpdate: CameraUpdate,
-        driver: Boolean = false
-    ) {
-        val markerRes = R.drawable.ic_map_label_a
-        val bmPinA = MapHelper.getPinBitmap(layoutInflater, placeName, info, markerRes)
-        val startMakerOptions = MarkerOptions().position(point).icon(BitmapDescriptorFactory.fromBitmap(bmPinA))
-        googleMap.addMarker(startMakerOptions)
-        googleMap.moveCamera(cameraUpdate)
     }
 
     override fun cancelReview() {
@@ -138,12 +80,6 @@ class RatingLastTripFragment : BaseBottomSheetDialogFragment(), RatingLastTripVi
                 .show(requireFragmentManager(), RatingDetailDialogFragment.RATE_DIALOG_TAG)
         }
     }
-
-    override fun blockInterface(block: Boolean, useSpinner: Boolean) {}
-
-    override fun setError(finish: Boolean, errId: Int, vararg args: String?) {}
-    override fun setError(e: ApiException) {}
-    override fun setError(e: DatabaseException) {}
 
     override fun setTransferNotFoundError(transferId: Long) {
         val act = activity
---
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/dialogs/BaseMapBottomSheetDialogFragment.kt
@@ -0,0 +1,84 @@
+package com.kg.gettransfer.presentation.ui.dialogs
+
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.kg.gettransfer.R
+import com.kg.gettransfer.domain.ApiException
+import com.kg.gettransfer.domain.DatabaseException
+import com.kg.gettransfer.presentation.model.RouteModel
+import com.kg.gettransfer.presentation.presenter.BaseMapDialogPresenter
+import com.kg.gettransfer.presentation.ui.Utils
+import com.kg.gettransfer.presentation.ui.helpers.MapHelper
+import com.kg.gettransfer.presentation.view.BaseMapDialogView
+
+abstract class BaseMapBottomSheetDialogFragment : BaseBottomSheetDialogFragment(), BaseMapDialogView, OnMapReadyCallback {
+
+    abstract fun getPresenter(): BaseMapDialogPresenter<*>
+
+    private lateinit var googleMap: GoogleMap
+
+    private var mapFragment: SupportMapFragment? = null
+
+    protected fun initMapFragment(layoutId: Int) {
+
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance()
+            (mapFragment as SupportMapFragment).getMapAsync(this)
+        }
+
+        childFragmentManager.beginTransaction().replace(layoutId, mapFragment as SupportMapFragment).commit()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        customizeGoogleMaps(googleMap)
+        getPresenter().onMapReady()
+    }
+
+    private fun customizeGoogleMaps(gm: GoogleMap) {
+        gm.uiSettings.isRotateGesturesEnabled = false
+        gm.uiSettings.isTiltGesturesEnabled = false
+        gm.uiSettings.isMyLocationButtonEnabled = false
+        gm.uiSettings.isScrollGesturesEnabled = false
+        gm.uiSettings.isZoomGesturesEnabled = false
+    }
+
+    override fun setRoute(routeModel: RouteModel?, from: String, startPoint: LatLng) {
+        if (routeModel != null) {
+            val polyline = Utils.getPolyline(routeModel)
+            if (MapHelper.isEmptyPolyline(polyline, routeModel)) {
+                return
+            } else {
+                context?.let { MapHelper.setPolyline(it, layoutInflater, googleMap, polyline, routeModel) }
+            }
+        } else {
+            setPinForHourlyTransfer(from, "", startPoint, Utils.getCameraUpdateForPin(startPoint))
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun setPinForHourlyTransfer(
+            placeName: String,
+            info: String,
+            point: LatLng,
+            cameraUpdate: CameraUpdate,
+            driver: Boolean = false
+    ) {
+        val markerRes = R.drawable.ic_map_label_a
+        val bmPinA = MapHelper.getPinBitmap(layoutInflater, placeName, info, markerRes)
+        val startMakerOptions = MarkerOptions().position(point).icon(BitmapDescriptorFactory.fromBitmap(bmPinA))
+        googleMap.addMarker(startMakerOptions)
+        googleMap.moveCamera(cameraUpdate)
+    }
+
+    override fun blockInterface(block: Boolean, useSpinner: Boolean) {}
+    override fun setError(finish: Boolean, errId: Int, vararg args: String?) {}
+    override fun setError(e: ApiException) {}
+    override fun setError(e: DatabaseException) {}
+    override fun setTransferNotFoundError(transferId: Long) {}
+}
