2ec9878a7d72a0509af772c1b8b6e5e2d95df2c1
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2019-11-06 19:45:02 +0300

    fix: opening selecting language and currency screens
    GAA-2042

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SelectLanguageFragment.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/SelectLanguageFragment.kt
@@ -1,6 +1,5 @@
 package com.kg.gettransfer.presentation.ui
 
-import android.animation.Animator
 import android.os.Bundle
 
 import androidx.annotation.CallSuper
@@ -10,7 +9,6 @@ import androidx.core.content.ContextCompat
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -24,7 +22,6 @@ import com.kg.gettransfer.extensions.setThrottledClickListener
 import com.kg.gettransfer.presentation.adapter.LanguagesListAdapter
 import com.kg.gettransfer.presentation.model.LocaleModel
 import com.kg.gettransfer.presentation.presenter.SelectLanguagePresenter
-import com.kg.gettransfer.presentation.ui.utils.FragmentUtils
 import com.kg.gettransfer.presentation.view.SelectLanguageView
 import kotlinx.android.synthetic.main.fragment_select_language.*
 import kotlinx.android.synthetic.main.layout_select_language.*
@@ -32,7 +29,6 @@ import kotlinx.android.synthetic.main.toolbar_nav_back.view.*
 
 open class SelectLanguageFragment : BaseBottomSheetFragment(), SelectLanguageView {
 
-    private lateinit var adapterAll: LanguagesListAdapter
     override val layout = R.layout.fragment_select_language
 
     @InjectPresenter
@@ -56,10 +52,6 @@ open class SelectLanguageFragment : BaseBottomSheetFragment(), SelectLanguageVie
         ContextCompat.getDrawable(requireContext(), R.drawable.sh_divider_light_gray)?.let { drawable ->
             itemDecorator.setDrawable(drawable)
         }
-
-        adapterAll = LanguagesListAdapter(presenter::changeLanguage)
-        rvAllLanguages.adapter = adapterAll
-        rvAllLanguages.itemAnimator = DefaultItemAnimator()
         rvAllLanguages.addItemDecoration(itemDecorator)
         setupToolbar()
     }
@@ -69,18 +61,8 @@ open class SelectLanguageFragment : BaseBottomSheetFragment(), SelectLanguageVie
         toolBar.toolbar.toolbar_title.text = getString(R.string.LNG_LANGUAGES)
     }
 
-    /**
-     * Update UI after finished start fragment
-     */
-    override fun onCreateAnimator(transit: Int, enter: Boolean, nextAnim: Int): Animator {
-        return FragmentUtils.onCreateAnimation(requireContext(), enter) {
-            adapterAll.notifyDataSetChanged()
-        }
-    }
-
     override fun setLanguages(all: List<LocaleModel>, selected: LocaleModel) {
-        adapterAll.setNewSelectedLanguage(selected)
-        adapterAll.update(all)
+        rvAllLanguages.adapter = LanguagesListAdapter(presenter::changeLanguage, all, selected)
     }
 
     override fun recreateActivity() {
