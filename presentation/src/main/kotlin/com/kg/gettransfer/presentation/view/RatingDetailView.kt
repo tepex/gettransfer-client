0cf3e6e4a7b6a1bb17778028cea5de761df81f21
Gromov Evgeny <e.gromov@key-g.com>
2019-11-21 13:28:44 +0000

    feat: prepare animation
    GAA-1559

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/RatingDetailView.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/view/RatingDetailView.kt
@@ -7,25 +7,27 @@ import com.kg.gettransfer.domain.model.ReviewRate
 
 interface RatingDetailView : BaseView {
 
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun showProgress(isShow: Boolean)
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun setRatingCommon(rating: Float)
 
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun setRatingCommon(rating: Float)
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun setRatingVehicle(rating: Float)
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun setRatingDriver(rating: Float)
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun setRatingPunctuality(rating: Float)
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun setDividersVisibility()
-	@StateStrategyType(AddToEndSingleStrategy::class)
-	fun showComment(comment: String)
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun setRatingVehicle(rating: Float)
 
-	@StateStrategyType(OneExecutionStateStrategy::class)
-	fun showCommentDialog(comment: String)
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun setRatingDriver(rating: Float)
 
-	@StateStrategyType(OneExecutionStateStrategy::class)
-	fun exitAndReportSuccess(list: List<ReviewRate>, comment: String)
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun setRatingPunctuality(rating: Float)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun setDividersVisibility()
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showComment(comment: String)
+
+    @StateStrategyType(OneExecutionStateStrategy::class)
+    fun showCommentDialog(comment: String)
+
+    @StateStrategyType(OneExecutionStateStrategy::class)
+    fun exitAndReportSuccess(list: List<ReviewRate>, comment: String)
 }
