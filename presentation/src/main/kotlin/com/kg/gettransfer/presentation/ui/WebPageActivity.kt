cb3bb0b3bb08b90deb3cb2ba850761bd90a96be9
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2020-01-22 15:59:06 +0300

    refactor: WebPageActivity

--- presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/WebPageActivity.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/presentation/ui/WebPageActivity.kt
@@ -7,9 +7,8 @@ import android.os.Bundle
 import androidx.annotation.CallSuper
 import androidx.annotation.StringRes
 
-import androidx.appcompat.widget.Toolbar
-
 import android.webkit.WebViewClient
+import androidx.core.view.isVisible
 
 import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
@@ -27,6 +26,7 @@ import com.kg.gettransfer.sys.domain.GetMobileConfigsInteractor
 import com.kg.gettransfer.sys.domain.GetPreferencesInteractor
 
 import kotlinx.android.synthetic.main.activity_web_page.*
+import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.android.synthetic.main.toolbar.view.*
 
 import kotlinx.coroutines.launch
@@ -40,8 +40,8 @@ import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.android.support.SupportAppNavigator
 
 class WebPageActivity : MvpAppCompatActivity(), WebPageView {
-    var navigator = SupportAppNavigator(this, Screens.NOT_USED)
-    val navigatorHolder: NavigatorHolder by inject()
+    private var navigator = SupportAppNavigator(this, Screens.NOT_USED)
+    private val navigatorHolder: NavigatorHolder by inject()
     private val worker: WorkerManager by inject { parametersOf("WebPage") }
     private val getMobileConfigs: GetMobileConfigsInteractor by inject()
     private val getPreferences: GetPreferencesInteractor by inject()
@@ -56,50 +56,9 @@ class WebPageActivity : MvpAppCompatActivity(), WebPageView {
     @CallSuper
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_web_page)
-
-        setSupportActionBar(toolbar as Toolbar)
-        supportActionBar?.apply {
-            setDisplayShowTitleEnabled(false)
-            setDisplayHomeAsUpEnabled(true)
-            setDisplayShowHomeEnabled(true)
-        }
-        (toolbar as Toolbar).setNavigationOnClickListener { presenter.onBackCommandClick() }
-
-        webView.settings.javaScriptEnabled = true
-        webView.setUserAgent()
-        webView.webViewClient = object : WebViewClient() {}
-
-        worker.main.launch {
-            val url = withContext(worker.bg) { getPreferences().getModel() }.endpoint!!.url
-            when (intent.getStringExtra(WebPageView.EXTRA_SCREEN)) {
-                WebPageView.SCREEN_LICENSE -> {
-                    val termsUrl = withContext(worker.bg) { getMobileConfigs().getModel() }.termsUrl
-                    initActivity(
-                        R.string.LNG_RIDE_OFFERT_TITLE,
-                        SystemUtils.getUrlWithLocale(url).plus("/$termsUrl")
-                    )
-                }
-                WebPageView.SCREEN_REG_CARRIER -> initActivity(
-                    R.string.LNG_RIDE_CREATE_CARRIER,
-                    SystemUtils.getUrlWithLocale(url).plus(getString(R.string.api_url_carrier_new))
-                )
-                WebPageView.SCREEN_CARRIER -> initActivity(
-                    R.string.LNG_RIDE_CREATE_CARRIER,
-                    SystemUtils.getUrlWithLocale(url).plus(getString(R.string.api_url_carrier))
-                )
-                WebPageView.SCREEN_RESTORE_PASS -> initActivity(
-                    R.string.LNG_LOGIN_RECOVERY_PASSWORD,
-                    SystemUtils.getUrlWithLocale(url).plus(getString(R.string.api_url_restore_password))
-                )
-                WebPageView.SCREEN_TRANSFERS -> initActivity(
-                    null,
-                    SystemUtils.getUrlWithLocale(url).plus(getString(R.string.api_url_transfers)),
-                    url
-                )
-            }
-        }
+        initToolbar()
+        initWebView()
     }
 
     @CallSuper
@@ -114,9 +73,58 @@ class WebPageActivity : MvpAppCompatActivity(), WebPageView {
         super.onPause()
     }
 
-    override fun initActivity(@StringRes title: Int?, strUrl: String, stringTitle: String?) {
-        (toolbar as Toolbar).toolbar_title.text = stringTitle ?: title?.let { getString(it) }
-        webView.loadUrl(strUrl)
+    private fun initToolbar() = with(toolbar) {
+        setSupportActionBar(this)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+            setDisplayShowHomeEnabled(true)
+        }
+        with(toolbar_btnBack) {
+            isVisible = true
+            setOnClickListener { presenter.onBackCommandClick() }
+        }
+    }
+
+    private fun initWebView() {
+        webView.settings.javaScriptEnabled = true
+        webView.setUserAgent()
+        webView.webViewClient = object : WebViewClient() {}
+
+        worker.main.launch {
+            val endpointUrl = withContext(worker.bg) { getPreferences().getModel() }.endpoint!!.url
+            val pathUrl = when (intent.getStringExtra(WebPageView.EXTRA_SCREEN)) {
+                WebPageView.SCREEN_LICENSE -> {
+                    val termsUrl = withContext(worker.bg) { getMobileConfigs().getModel() }.termsUrl
+                    setToolbarTitle(R.string.LNG_RIDE_OFFERT_TITLE)
+                    "/$termsUrl"
+                }
+                WebPageView.SCREEN_REG_CARRIER -> {
+                    setToolbarTitle(R.string.LNG_RIDE_CREATE_CARRIER)
+                    getString(R.string.api_url_carrier_new)
+                }
+                WebPageView.SCREEN_CARRIER -> {
+                    setToolbarTitle(R.string.LNG_RIDE_CREATE_CARRIER)
+                    getString(R.string.api_url_carrier)
+                }
+                WebPageView.SCREEN_RESTORE_PASS -> {
+                    setToolbarTitle(R.string.LNG_LOGIN_RECOVERY_PASSWORD)
+                    getString(R.string.api_url_restore_password)
+                }
+                WebPageView.SCREEN_TRANSFERS -> {
+                    setToolbarTitle(titleText = endpointUrl)
+                    getString(R.string.api_url_transfers)
+                }
+                else -> null
+            }
+            val webViewUrl = SystemUtils.getUrlWithLocale(endpointUrl).let { baseUrl ->
+                pathUrl?.let { baseUrl.plus(pathUrl) } ?: baseUrl
+            }
+            webView.loadUrl(webViewUrl)
+        }
+    }
+
+    private fun setToolbarTitle(@StringRes titleId: Int? = null, titleText: String? = null) {
+        toolbar_title.text = titleText ?: titleId?.let { getString(it) }
     }
 
     override fun onBackPressed() {
