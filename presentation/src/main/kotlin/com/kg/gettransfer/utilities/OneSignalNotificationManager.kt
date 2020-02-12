838d7367e0844b963af226972d20cdd7a8feb1ca
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2020-02-13 01:38:27 +0300

    fix: changed groupKey for OneSignal notifications

--- presentation/src/main/kotlin/com/kg/gettransfer/utilities/OneSignalNotificationManager.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/utilities/OneSignalNotificationManager.kt
@@ -9,15 +9,15 @@ import org.koin.core.KoinComponent
 class OneSignalNotificationManager(val context: Context) : ContextWrapper(context), KoinComponent {
 
     companion object {
-        const val OFFER_CHANEL_PREFIX   = "offer_"
-        const val MESSAGE_CHANEL_PREFIX = "message_"
+        const val OFFER_CHANEL_POSTFIX   = "_offer"
+        const val MESSAGE_CHANEL_POSTFIX = "_message"
     }
 
     fun clearOfferNotifications(transferId: Long) {
-        OneSignal.cancelGroupedNotifications(OFFER_CHANEL_PREFIX.plus(transferId))
+        OneSignal.cancelGroupedNotifications(transferId.toString().plus(OFFER_CHANEL_POSTFIX))
     }
 
     fun clearChatNotifications(transferId: Long) {
-        OneSignal.cancelGroupedNotifications(MESSAGE_CHANEL_PREFIX.plus(transferId))
+        OneSignal.cancelGroupedNotifications(transferId.toString().plus(MESSAGE_CHANEL_POSTFIX))
     }
 }
