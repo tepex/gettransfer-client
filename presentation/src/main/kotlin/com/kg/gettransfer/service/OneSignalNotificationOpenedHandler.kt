a49f4a125305229f49f425a779562692b094ca48
Viacheslav Andrushkevich <v.andrushkevich@key-g.com>
2020-02-13 17:42:19 +0300

    feat: added navigations for OneSignal notifications
    GAA-2259

--- presentation/src/main/kotlin/com/kg/gettransfer/service/OneSignalNotificationOpenedHandler.kt
+++ presentation/src/main/kotlin/com/kg/gettransfer/service/OneSignalNotificationOpenedHandler.kt
@@ -4,60 +4,183 @@ import android.app.Application
 import android.content.Intent
 import android.net.Uri
 import androidx.core.net.toUri
-import com.kg.gettransfer.domain.interactor.SessionInteractor
-import com.kg.gettransfer.presentation.ui.HandleUrlActivity
-import com.kg.gettransfer.presentation.ui.OffersActivity
-import com.kg.gettransfer.presentation.view.OffersView
+import com.kg.gettransfer.core.presentation.WorkerManager
+import com.kg.gettransfer.domain.interactor.*
+import com.kg.gettransfer.domain.model.OfferItem
+import com.kg.gettransfer.domain.model.Transfer
+import com.kg.gettransfer.extensions.getOffer
+import com.kg.gettransfer.presentation.model.DeeplinkScreenModel
+import com.kg.gettransfer.presentation.ui.*
+import com.kg.gettransfer.presentation.view.*
+import com.kg.gettransfer.utilities.DeeplinkManager
 import com.onesignal.OSNotificationOpenResult
 import com.onesignal.OneSignal
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
 
 class OneSignalNotificationOpenedHandler(
     private val application: Application
 ) : OneSignal.NotificationOpenedHandler, KoinComponent {
 
+    val worker: WorkerManager by inject { parametersOf("OneSignalNotificationOpenedHandler") }
+
+    private val deeplinkManager: DeeplinkManager by inject()
+
     private val sessionInteractor: SessionInteractor by inject()
+    private val orderInteractor: OrderInteractor by inject()
+    private val transferInteractor: TransferInteractor by inject()
+    private val offerInteractor: OfferInteractor by inject()
+    private val paymentInteractor: PaymentInteractor by inject()
 
     override fun notificationOpened(result: OSNotificationOpenResult) {
-        val uri = result.notification.payload.launchURL.toUri()
-
-        val intent = getIntentForUrl(uri)
-        application.startActivity(intent)
+        result.notification.payload.launchURL?.let { url ->
+            checkUrlData(url.toUri())
+        } ?: openMainActivity()
     }
 
-    private fun getIntentForUrl(uri: Uri): Intent {
-        val fragment = uri.fragment ?: return getHandleUrlActivityIntent(uri)
+    private fun checkUrlData(appLinkData: Uri) {
+        if (!sessionInteractor.isInitialized) {
+            openHandleUrlActivity(appLinkData)
+            return
+        }
 
-        val isCabinetUrl = uri.path == PASSENGER_CABINET || uri.path == PARTNER_CABINET
-        val isOfferPaymentUrl = fragment.contains(CHOOSE_OFFER_ID)
-        val isChatUrl = fragment.contains(OPEN_CHAT)
+        when(val screen = deeplinkManager.getScreenForLink(appLinkData)) {
+            is DeeplinkScreenModel.DownloadVoucher -> openHandleUrlActivity(appLinkData)
+            is DeeplinkScreenModel.NewPassword -> openProfileSettingsActivity()
+            is DeeplinkScreenModel.Main -> openMainActivity()
+            is DeeplinkScreenModel.CreateOrder -> checkCreateOrderLink(screen)
 
-        val transferId = fragment.substring(fragment.indexOf(SLASH) + 1).toLongOrNull()
-        val isOpenOffersScreen = isCabinetUrl && !isOfferPaymentUrl && !isChatUrl && transferId != null
-        return if (sessionInteractor.isInitialized && isOpenOffersScreen) {
-            Intent(application, OffersActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                putExtra(OffersView.EXTRA_TRANSFER_ID, transferId)
-                putExtra(OffersView.EXTRA_ORIGIN, OffersView.SOURCE_NOTIFICATION)
-            }
-        } else {
-            getHandleUrlActivityIntent(uri)
+            is DeeplinkScreenModel.PaymentOffer -> checkPaymentOfferLink(screen.transferId, screen.offerId, screen.bookNowOfferId)
+            is DeeplinkScreenModel.Chat -> openChatActivity(screen.transferId)
+            is DeeplinkScreenModel.Transfer -> checkTransferLink(screen.transferId)
+            is DeeplinkScreenModel.RateTransfer -> openMainActivityForRateTransfer(screen.transferId, screen.rate)
         }
     }
 
-    private fun getHandleUrlActivityIntent(uri: Uri) =
-        Intent(application, HandleUrlActivity::class.java).apply {
+    private fun checkCreateOrderLink(screen: DeeplinkScreenModel.CreateOrder) = worker.main.launch {
+        with(orderInteractor) {
+            screen.fromPlaceId?.let { withContext(worker.bg) { updatePoint(false, it) } }
+            screen.toPlaceId?.let   { withContext(worker.bg) { updatePoint(true, it) } }
+            screen.promo?.let { promoCode = it }
+            if (isCanCreateOrder()) {
+                openCreateOrderActivity()
+            } else {
+                openMainActivity()
+            }
+        }
+    }
+
+    private fun checkPaymentOfferLink(transferId: Long, offerId: Long?, bookNowTransportId: String?) = worker.main.launch {
+        getTransfer(transferId)?.let { transfer ->
+            val offerItem: OfferItem? = when {
+                offerId != null && offerId != 0L ->
+                    withContext(worker.bg) { offerInteractor.getOffers(transfer.id) }.model.getOffer(offerId)
+                !bookNowTransportId.isNullOrEmpty()      ->
+                    transfer.bookNowOffers.find { it.transportType.id.toString() == bookNowTransportId }
+                else                                     -> null
+            }
+            if (offerItem != null) {
+                with(paymentInteractor) {
+                    selectedTransfer = transfer
+                    selectedOffer = offerItem
+                }
+                openPaymentOfferActivity()
+            } else {
+                openMainActivity()
+            }
+        }
+    }
+
+    private fun checkTransferLink(transferId: Long) = worker.main.launch {
+        getTransfer(transferId)?.let { transfer ->
+            if (transfer.checkStatusCategory() == Transfer.STATUS_CATEGORY_ACTIVE) {
+                openOffersActivity(transferId)
+            } else {
+                openTransferDetailsActivity(transferId)
+            }
+        }
+    }
+
+    private fun openHandleUrlActivity(uri: Uri) {
+        val intent = Intent(application, HandleUrlActivity::class.java).apply {
             action = Intent.ACTION_VIEW
             data = uri
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
+        application.startActivity(intent)
+    }
 
-    companion object {
-        const val PASSENGER_CABINET = "/passenger/cabinet"
-        const val PARTNER_CABINET = "/partner/cabinet"
-        const val CHOOSE_OFFER_ID = "choose_offer_id"
-        const val OPEN_CHAT = "open_chat"
-        const val SLASH = "/"
+    private fun openProfileSettingsActivity() {
+        val intent = Intent(application, ProfileSettingsActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openMainActivity() {
+        val intent = Intent(application, MainNavigateActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openCreateOrderActivity() {
+        val intent = Intent(application, CreateOrderActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openPaymentOfferActivity() {
+        val intent = Intent(application, PaymentOfferActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openChatActivity(transferId: Long) {
+        val intent = Intent(application, ChatActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            putExtra(ChatView.EXTRA_TRANSFER_ID, transferId)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openOffersActivity(transferId: Long) {
+        val intent = Intent(application, OffersActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            putExtra(OffersView.EXTRA_TRANSFER_ID, transferId)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openTransferDetailsActivity(transferId: Long) {
+        val intent = Intent(application, TransferDetailsActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            putExtra(TransferDetailsView.EXTRA_TRANSFER_ID, transferId)
+        }
+        application.startActivity(intent)
+    }
+
+    private fun openMainActivityForRateTransfer(transferId: Long, rate: Int?) {
+        val intent = Intent(application, MainNavigateActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            putExtra(MainNavigateView.EXTRA_RATE_TRANSFER_ID, transferId)
+            putExtra(MainNavigateView.EXTRA_RATE_VALUE, rate)
+        }
+        application.startActivity(intent)
+    }
+
+    private suspend fun getTransfer(transferId: Long): Transfer? {
+        val result = withContext(worker.bg) { transferInteractor.getTransfer(transferId) }
+        return if (result.isError()) {
+            openMainActivity()
+            null
+        } else {
+            result.model
+        }
     }
 }
